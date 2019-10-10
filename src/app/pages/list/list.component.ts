983aa9dad688fbc0e467d48feabf6cda56bf23e1
Pratik <bhadiyadrapratik007@gmail.com>
2019-10-10 19:45:25 +0530

    1.change 'select merchant' with 'select user' in all pages of business menu. 2.change 'name' to 'Trade Name' in list page.

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -45,7 +45,7 @@ export class ListComponent implements OnInit {
   admin_list: any = [];
   filterData:any = [];
   filters_list: any = [ 
-    {'in_prod_name':'Name'},
+    {'in_prod_name':'Trade Name'},
     {'in_prod_name':'Mobile Number'},
     {'in_prod_name':'Document Type'},
     {'in_prod_name':'Status of Invoice'},
@@ -90,7 +90,7 @@ export class ListComponent implements OnInit {
         for(var i=0;i<event.length;i++) {
           var it = event[i];      
           if(it.attr == 'Mobile Number' && it.value && rd.merchantMobileNumber.toLowerCase().indexOf(it.value.toLowerCase()) == -1 || 
-            it.attr == 'Name' && it.value && rd.merchantName.toLowerCase().indexOf(it.value.toLowerCase()) == -1 || 
+            it.attr == 'Trade Name' && it.value && rd.merchantName.toLowerCase().indexOf(it.value.toLowerCase()) == -1 || 
             it.attr == 'Document Type' && it.value && rd.invoiceDocumentType && rd.invoiceDocumentType.toLowerCase().indexOf(it.value.toLowerCase()) == -1  ||
             it.attr == 'Status of Invoice' && it.value && rd.invoiceStatus && rd.invoiceStatus.toLowerCase().indexOf(it.value.toLowerCase()) == -1 ||
             it.attr == 'Owner' && it.value && rd.processedBy && rd.processedBy.toLowerCase().indexOf(it.value.toLowerCase()) == -1
