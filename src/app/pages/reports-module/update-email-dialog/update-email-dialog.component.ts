dd453776505a5067d257a0397af15132f488d33b
ashishhulwan <ashish.hulwan@ssbainnovations.com>
2019-11-20 15:29:07 +0530

    Cognito

--- src/app/pages/reports-module/update-email-dialog/update-email-dialog.component.ts
+++ src/app/pages/reports-module/update-email-dialog/update-email-dialog.component.ts
@@ -7,35 +7,40 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-update-email-dialog',
   templateUrl: './update-email-dialog.component.html',
   styleUrls: ['./update-email-dialog.component.css'],
-  providers:[UserMsService]
+  providers: [UserMsService]
 })
 export class UpdateEmailDialogComponent extends SimpleModalComponent<ConfirmModel, boolean> implements ConfirmModel, OnInit {
   title: string;
   message: string;
-  userData:any;
+  userData: any;
   emailAddress: string = '';
-  constructor( private http: HttpClient,private userMsService: UserMsService,) {
+  busy: Boolean = false;
+  constructor(private http: HttpClient, private userMsService: UserMsService, ) {
     super();
   }
 
   ngOnInit() {
   }
-  updateEmail(){
+  updateEmail() {
+    this.busy = false;
     const userParam = `/profile/${this.userData.userId}`;
-    if(this.emailAddress){
-    this.userData.emailAddress = this.emailAddress
+    if (this.emailAddress) {
+      this.busy = true;
+      this.userData.emailAddress = this.emailAddress
       this.userMsService.putMethod(userParam, this.userData).subscribe((res: any) => {
+        this.busy = false;
         this.result = true;
         this.close();
         console.log('Email update success:', res);
       }, error => {
+        this.busy = false;
         console.log('Email update failure:', error);
       })
+    }
   }
-}
 }
 export interface ConfirmModel {
   title: string;
   message: string;
-  userData:any;
+  userData: any;
 }
