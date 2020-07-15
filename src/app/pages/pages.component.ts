cf247b89ae1474f8c4cd26d13ad7e5c25ca6a48f
BhondaveSagarSharad <sagar@ssbainnovations.com>
2020-07-15 20:18:50 +0530

    Notification popup issue resolved

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -46,10 +46,10 @@ export class PagesComponent implements OnInit {
       }
     })
 
-    // this.router.events.subscribe((url:any) => {
-    //   console.log('Path: ', router.url)
-    //     this.routePath = router.url;
-    //  }); 
+    this.router.events.subscribe((url:any) => {
+      console.log('Path: ', router.url)
+        this.routePath = router.url;
+     }); 
   }
 
   ngOnInit() {
