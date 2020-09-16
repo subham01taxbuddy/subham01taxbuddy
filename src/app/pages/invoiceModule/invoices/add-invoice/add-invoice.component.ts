04dd41010dd1a5a5bececd24290a5dffe7c691ce
BhondaveSagarSharad <sagar@ssbainnovations.com>
2020-09-16 22:14:59 +0530

    validation added to bill to field in Invoice

--- src/app/pages/invoiceModule/invoices/add-invoice/add-invoice.component.ts
+++ src/app/pages/invoiceModule/invoices/add-invoice/add-invoice.component.ts
@@ -111,7 +111,7 @@ export class AddInvoiceComponent implements OnInit {
       sacCode: ['998232', Validators.required],
       cin: ['U74999MH2017PT298565', Validators.required],
       modeOfPayment: ['Online', Validators.required],
-      billTo: ['', Validators.required],
+      billTo: ['', [Validators.required,Validators.pattern(AppConstants.charRegex)]],
       paymentCollectedBy: '',
       dateOfReceipt: '',
       dateOfDeposit: '',
