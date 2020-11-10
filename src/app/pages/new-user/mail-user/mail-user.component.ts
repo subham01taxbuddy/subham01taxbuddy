d487eb6c1c7193d21fd6aa31b710aa5b62e6b819
BhondaveSagarSharad <sagar@ssbainnovations.com>
2020-11-10 16:09:10 +0530

    exception uset list issue solved

--- src/app/pages/new-user/mail-user/mail-user.component.ts
+++ src/app/pages/new-user/mail-user/mail-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserMsService } from 'app/services/user-ms.service';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-mail-user',
@@ -10,28 +11,25 @@ export class MailUserComponent implements OnInit {
 
   loading: boolean;
   showMailUser: boolean;
-  agentList = [
-    { value: 'brij@ssbainnovations.com', label: 'Brij' },
-    { value: 'divya@ssbainnovations.com', label: 'Divya' },
-    { value: 'urmila@ssbainnovations.com', label: 'Urmila' },
-    { value: 'kavita@ssbainnovations.com', label: 'Kavita' },
-    { value: 'amrita@ssbainnovations.com', label: 'Amrita' },
-    { value: 'ankita@ssbainnovations.com', label: 'Ankita' },
-    { value: 'roshan.kakade@taxbuddy.com', label: 'Roshan' },
-    { value: 'damini@ssbainnovations.com', label: 'Damini' }
-  ];
+  agentList = environment.agentId;
+
   mailUser: any = [];
 
   constructor(private userService: UserMsService) { }
 
   ngOnInit() {
-    //this.getMailUserByAgentId();
+    this.getMailUserByAgentId();
   }
 
-  getMailUserByAgentId(agentId){
+  getMailUserByAgentId(agentId?){
     this.showMailUser = true;
     this.loading = true;
-    let param = '/email-channel?assigneeId='+agentId;
+    if(agentId){
+      var param = '/email-channel?assigneeId='+agentId;
+    }else{
+      var param = '/email-channel?assigneeId=';
+    }
+    
     this.userService.getUserDetail(param).subscribe(responce=>{
       this.loading = false;
        console.log('Email user ==> ',responce);
