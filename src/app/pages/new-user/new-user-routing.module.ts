cd40744d9f578646c50bc01c378705cb3f769b55
BhondaveSagarSharad <sagar@ssbainnovations.com>
2020-11-27 19:02:59 +0530

    Edit user part added

--- src/app/pages/new-user/new-user-routing.module.ts
+++ src/app/pages/new-user/new-user-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CreateUserComponent } from "./create-user/create-user.component";
+import { EditUserComponent } from "./edit-user/edit-user.component";
 import { MailExceptionComponent } from "./mail-exception/mail-exception.component";
 import { MailUserComponent } from "./mail-user/mail-user.component";
 import { NewUserWizardComponent } from "./new-user-wizard/new-user-wizard.component";
@@ -11,6 +12,7 @@ const routes: Routes= [
         {path: 'createUser', component: CreateUserComponent},
         {path: 'mail-user', component: MailUserComponent},
         {path: 'mail-exception', component: MailExceptionComponent},
+        {path: 'editUser', component: EditUserComponent},
         {path: '', redirectTo: '/pages/newUser/mail-user', pathMatch: 'full' },
      ]
   },
--- src/app/pages/new-user/new-user.module.ts
+++ src/app/pages/new-user/new-user.module.ts
@@ -9,9 +9,10 @@ import { NewUserRoutingModule } from "./new-user-routing.module";
 import { NewUserWizardComponent } from './new-user-wizard/new-user-wizard.component';
 import { MailUserComponent } from './mail-user/mail-user.component';
 import { MailExceptionComponent } from './mail-exception/mail-exception.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 @NgModule({
-    declarations: [CreateUserComponent, NewUserWizardComponent, MailUserComponent, MailExceptionComponent],
+    declarations: [CreateUserComponent, NewUserWizardComponent, MailUserComponent, MailExceptionComponent, EditUserComponent],
     imports: [
         CommonModule,
         FormsModule,
