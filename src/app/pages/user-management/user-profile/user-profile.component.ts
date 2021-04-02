74d669601f6f7319175065d4f26d49b884187bdf
BhondaveSagarSharad <sagar@ssbainnovations.com>
2021-04-02 19:17:16 +0530

    state name issue solved in userProfile

--- src/app/pages/user-management/user-profile/user-profile.component.ts
+++ src/app/pages/user-management/user-profile/user-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material';
+import { DateAdapter, MatDialog, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { GstMsService } from 'app/services/gst-ms.service';
 import { ThirdPartyService } from 'app/services/third-party.service';
@@ -13,12 +13,28 @@ import { reject } from 'lodash';
 import { ProfileDialogComponent } from '../profile-dialog/profile-dialog.component';
 import { TitleCasePipe } from '@angular/common';
 import Storage from '@aws-amplify/storage';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
-  providers: [TitleCasePipe]
+  providers: [TitleCasePipe, { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
+  ]
 })
 export class UserProfileComponent implements OnInit {
 
@@ -27,7 +43,6 @@ export class UserProfileComponent implements OnInit {
   userProfileForm: FormGroup;
   gstForm: FormGroup;
   gstType: any = [{ label: 'Regular', value: 'REGULAR' }, { label: 'Composite', value: 'COMPOSITE' }, { label: 'Input Service Distributor (ISD)', value: 'Input Service Distributor (ISD)' }]
-  state_list: any = [];
   gstr1List: any = [{ label: 'Monthly', value: 'MONTHLY' }, { label: 'Quarterly', value: 'QUARTERLY' }];
   genderData: any = [{ label: 'Male', value: 'MALE' }, { label: 'Female', value: 'FEMALE' }]
   marritalStatusData: any = [{ label: 'Single', value: 'SINGLE' }, { label: 'Married', value: 'MARRIED' }];
@@ -37,7 +52,267 @@ export class UserProfileComponent implements OnInit {
     // { value: 'NON_ORDINARY', label: 'Non Ordinary Resident' }
   ];
   bankData: any = [];
-  addressTypeData: any = [{ label: 'Home', value: 'HOME' }, { label: 'Office', value: 'OFFICE' }]
+  addressTypeData: any = [{ label: 'Home', value: 'HOME' }, { label: 'Office', value: 'OFFICE' }];
+  state_list = [{
+    "id": "5b4599c9c15a76370a3424c2",
+    "stateId": "1",
+    "countryCode": "91",
+    "stateName": "Andaman and Nicobar Islands",
+    "stateCode": "01",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c3",
+    "stateId": "2",
+    "countryCode": "91",
+    "stateName": "Andhra Pradesh",
+    "stateCode": "02",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c4",
+    "stateId": "3",
+    "countryCode": "91",
+    "stateName": "Arunachal Pradesh",
+    "stateCode": "03",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c5",
+    "stateId": "4",
+    "countryCode": "91",
+    "stateName": "Assam",
+    "stateCode": "04",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c6",
+    "stateId": "5",
+    "countryCode": "91",
+    "stateName": "Bihar",
+    "stateCode": "05",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c7",
+    "stateId": "6",
+    "countryCode": "91",
+    "stateName": "Chandigarh",
+    "stateCode": "06",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c8",
+    "stateId": "7",
+    "countryCode": "91",
+    "stateName": "Chattisgarh",
+    "stateCode": "33",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424c9",
+    "stateId": "8",
+    "countryCode": "91",
+    "stateName": "Dadra Nagar and Haveli",
+    "stateCode": "07",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424ca",
+    "stateId": "9",
+    "countryCode": "91",
+    "stateName": "Daman and Diu",
+    "stateCode": "08",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424cb",
+    "stateId": "10",
+    "countryCode": "91",
+    "stateName": "Delhi",
+    "stateCode": "09",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424cc",
+    "stateId": "11",
+    "countryCode": "91",
+    "stateName": "Goa",
+    "stateCode": "10",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424cd",
+    "stateId": "12",
+    "countryCode": "91",
+    "stateName": "Gujarat",
+    "stateCode": "11",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424ce",
+    "stateId": "13",
+    "countryCode": "91",
+    "stateName": "Haryana",
+    "stateCode": "12",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424cf",
+    "stateId": "14",
+    "countryCode": "91",
+    "stateName": "Himachal Pradesh",
+    "stateCode": "13",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d0",
+    "stateId": "15",
+    "countryCode": "91",
+    "stateName": "Jammu and Kashmir",
+    "stateCode": "14",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d1",
+    "stateId": "16",
+    "countryCode": "91",
+    "stateName": "Jharkhand",
+    "stateCode": "35",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d2",
+    "stateId": "17",
+    "countryCode": "91",
+    "stateName": "Karnataka",
+    "stateCode": "15",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d3",
+    "stateId": "18",
+    "countryCode": "91",
+    "stateName": "Kerala",
+    "stateCode": "16",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d4",
+    "stateId": "19",
+    "countryCode": "91",
+    "stateName": "Lakshadweep",
+    "stateCode": "17",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d5",
+    "stateId": "20",
+    "countryCode": "91",
+    "stateName": "Madhya Pradesh",
+    "stateCode": "18",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d6",
+    "stateId": "21",
+    "countryCode": "91",
+    "stateName": "Maharashtra",
+    "stateCode": "19",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d7",
+    "stateId": "22",
+    "countryCode": "91",
+    "stateName": "Manipur",
+    "stateCode": "20",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d8",
+    "stateId": "23",
+    "countryCode": "91",
+    "stateName": "Meghalaya",
+    "stateCode": "21",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424d9",
+    "stateId": "24",
+    "countryCode": "91",
+    "stateName": "Mizoram",
+    "stateCode": "22",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424da",
+    "stateId": "25",
+    "countryCode": "91",
+    "stateName": "Nagaland",
+    "stateCode": "23",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424db",
+    "stateId": "26",
+    "countryCode": "91",
+    "stateName": "Orissa",
+    "stateCode": "24",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424dc",
+    "stateId": "27",
+    "countryCode": "91",
+    "stateName": "Pondicherry",
+    "stateCode": "25",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424dd",
+    "stateId": "28",
+    "countryCode": "91",
+    "stateName": "Punjab",
+    "stateCode": "26",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424de",
+    "stateId": "29",
+    "countryCode": "91",
+    "stateName": "Rajasthan",
+    "stateCode": "27",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424df",
+    "stateId": "30",
+    "countryCode": "91",
+    "stateName": "Sikkim",
+    "stateCode": "28",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e0",
+    "stateId": "31",
+    "countryCode": "91",
+    "stateName": "Tamil Nadu",
+    "stateCode": "29",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e1",
+    "stateId": "32",
+    "countryCode": "91",
+    "stateName": "Telangana",
+    "stateCode": "36",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e2",
+    "stateId": "33",
+    "countryCode": "91",
+    "stateName": "Tripura",
+    "stateCode": "30",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e3",
+    "stateId": "34",
+    "countryCode": "91",
+    "stateName": "Uttar Pradesh",
+    "stateCode": "31",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e4",
+    "stateId": "35",
+    "countryCode": "91",
+    "stateName": "Uttarakhand",
+    "stateCode": "34",
+    "status": true
+  }, {
+    "id": "5b4599c9c15a76370a3424e5",
+    "stateId": "36",
+    "countryCode": "91",
+    "stateName": "West Bengal",
+    "stateCode": "32",
+    "status": true
+  }, {
+    "id": "5dc24c9779332f0ddccb7aa4",
+    "stateId": "37",
+    "countryCode": "91",
+    "stateName": "Ladakh",
+    "stateCode": "37",
+    "status": true
+  }]
 
   get getAddressArray() {
     return <FormArray>this.userProfileForm.get('address');
@@ -48,12 +323,12 @@ export class UserProfileComponent implements OnInit {
     private dialog: MatDialog, private titleCasePipe: TitleCasePipe) { }
 
   ngOnInit() {
-    this.getStateInfo().then(res => {
+    // this.getStateInfo().then(res => {
       this.activatedRoute.params.subscribe(params => {
         console.log("99999999999999999:", params)
         this.getUserInfo(params['id']);
       });
-    })
+    // })
 
     this.userProfileForm = this.fb.group({
       fName: [''],
@@ -138,6 +413,7 @@ export class UserProfileComponent implements OnInit {
       const param = '/pincode/' + pinCode.value;
       this.userService.getMethod(param).subscribe((result: any) => {
         const addressData = <FormArray>this.userProfileForm.get('address');
+        console.log('After pinCode add -> ',this.updateAddressForm(result))
         addressData.insert(0, this.updateAddressForm(result))
         addressData.removeAt(1)
         // this.userProfileForm.controls['country'].setValue('INDIA');   //91
@@ -154,7 +430,7 @@ export class UserProfileComponent implements OnInit {
 
   updateAddressForm(obj) {
     return this.fb.group({
-      state: [obj.stateName || ''],
+      state: [obj.stateCode || ''],
       city: [obj.taluka || ''],
       country: ['INDIA' || ''],
 
@@ -167,24 +443,24 @@ export class UserProfileComponent implements OnInit {
     })
   }
 
-  getStateInfo() {
-    return new Promise((resolve, reject) => {
-      this.state_list = [];
-      let param = '/state-masters'
-      this.gstService.getMethod(param).subscribe(res => {
-        if (Array.isArray(res)) {
-          res.forEach(sData => { sData.name = sData.stateMasterName });
-          this.state_list = res;
-        }
-        resolve(true)
-      },
-        error => {
-          let errorMessage = (error.error && error.error.message) ? error.error.message : "Internal server error.";
-          this._toastMessageService.alert("error", "state list - " + errorMessage);
-          resolve(false);
-        })
-    })
-  }
+  // getStateInfo() {
+  //   return new Promise((resolve, reject) => {
+  //     this.state_list = [];
+  //     let param = '/state-masters'
+  //     this.gstService.getMethod(param).subscribe(res => {
+  //       if (Array.isArray(res)) {
+  //         res.forEach(sData => { sData.name = sData.stateMasterName });
+  //         this.state_list = res;
+  //       }
+  //       resolve(true)
+  //     },
+  //       error => {
+  //         let errorMessage = (error.error && error.error.message) ? error.error.message : "Internal server error.";
+  //         this._toastMessageService.alert("error", "state list - " + errorMessage);
+  //         resolve(false);
+  //       })
+  //   })
+  // }
 
   getUserInfo(userId) {
     this.loading = true;
@@ -289,7 +565,7 @@ export class UserProfileComponent implements OnInit {
         let splitData = files[0].name.split(".");
         extention = "." + splitData[splitData.length - 1];
       }
-      Storage.put('business-signature/bsignature_' + this.userInfo.userId + "_" + new Date().getTime() + extention, files[0], {
+      Storage.put('business-logo/blogo_' + this.userInfo.userId + "_" + new Date().getTime() + extention, files[0], {
         contentType: files[0].type
       })
         .then((result: any) => {
