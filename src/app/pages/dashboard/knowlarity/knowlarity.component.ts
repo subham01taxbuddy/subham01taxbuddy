f032895000faa35b4816d8b557bc2e03b5772fe0
BhondaveSagarSharad <sagar@ssbainnovations.com>
2021-08-16 13:49:50 +0530

    working on knowlarity table

--- src/app/pages/dashboard/knowlarity/knowlarity.component.ts
+++ src/app/pages/dashboard/knowlarity/knowlarity.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { GridOptions } from 'ag-grid-community';
+import { UtilsService } from 'app/services/utils.service';
 
 @Component({
   selector: 'app-knowlarity',
@@ -11,7 +13,7 @@ export class KnowlarityComponent implements OnInit {
   loading: boolean;
   inbondKnowlarityGridOption: GridOptions;
 
-  constructor() {
+  constructor(private utilService: UtilsService,  @Inject(LOCALE_ID) private locale: string) {
     this.inbondKnowlarityGridOption = <GridOptions>{
       rowData: [],
       columnDefs: this.createColoumnDef(),
@@ -23,14 +25,29 @@ export class KnowlarityComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.getKnowlarityInfo();
+  }
+
+  getKnowlarityInfo(){
+    let knowlarityInfo = JSON.parse(sessionStorage.getItem('INBOND_KNOWLARITY'));
+    console.log('knowlarityInfo -> ',knowlarityInfo)
+    // if(this.utilService.isNonEmpty(knowlarityInfo)){
+    if(knowlarityInfo instanceof Array && knowlarityInfo.length > 0){
+      this.inbondKnowlarityGridOption.api.setRowData(this.createRowData(knowlarityInfo))
+    }
   }
 
   createColoumnDef(){
-    return [ {
+    return [
+     {
       headerName: 'Call Date',
-      field: 'userId',
-      width: 100,
+      field: 'callDate',
+      width: 150,
       suppressMovable: true,
+        cellStyle: { textAlign: 'center', 'fint-weight': 'bold' },
+        cellRenderer: (data) => {
+          return formatDate(data.value, 'dd/MM/yyyy', this.locale)
+        },
       filter: "agTextColumnFilter",
       filterParams: {
         filterOptions: ["contains", "notContains"],
@@ -40,7 +57,18 @@ export class KnowlarityComponent implements OnInit {
     {
       headerName: 'Call Time',
       field: 'callTime',
-      width: 100,
+      width: 150,
+      suppressMovable: true,
+      filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["contains", "notContains"],
+        debounceMs: 0
+      }
+    },
+    {
+      headerName: 'Call Type',
+      field: 'callType',
+      width: 180,
       suppressMovable: true,
       filter: "agTextColumnFilter",
       filterParams: {
@@ -51,7 +79,7 @@ export class KnowlarityComponent implements OnInit {
     {
       headerName: 'Agent Number',
       field: 'agentNumber',
-      width: 100,
+      width: 150,
       suppressMovable: true,
       filter: "agTextColumnFilter",
       filterParams: {
@@ -60,16 +88,45 @@ export class KnowlarityComponent implements OnInit {
       }
     },
     {
-      headerName: 'User Id',
-      field: 'userId',
-      width: 100,
+      headerName: 'Caller Number',
+      field: 'callerNumber',
+      width: 150,
       suppressMovable: true,
       filter: "agTextColumnFilter",
       filterParams: {
         filterOptions: ["contains", "notContains"],
         debounceMs: 0
       }
-    },]
+    },
+    {
+      headerName: 'Knowlarity Number',
+      field: 'knowlarity_number',
+      width: 180,
+      suppressMovable: true,
+      filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["contains", "notContains"],
+        debounceMs: 0
+      }
+    }]
+  }
+
+  createRowData(knowlarity){
+    console.log('knowlarity -> ', knowlarity);
+    var knowlaritysArray = [];
+    for (let i = 0; i < knowlarity.length; i++) {  
+      let knowlaritysInfo = Object.assign({}, knowlaritysArray[i], {
+        callDate: this.utilService.isNonEmpty(knowlarity[i]['Event_Date_Local']) ? knowlarity[i]['Event_Date_Local'] : '-',
+        callTime: this.utilService.isNonEmpty(knowlarity[i]['Caller_Channel_Created_Time']) ? knowlarity[i]['Caller_Channel_Created_Time'] : '-',
+        callType: this.utilService.isNonEmpty(knowlarity[i]['business_call_type']) ? knowlarity[i]['business_call_type'] : '-',
+        agentNumber: this.utilService.isNonEmpty(knowlarity[i]['agent_number']) ? knowlarity[i]['agent_number'] : '-',
+        callerNumber: this.utilService.isNonEmpty(knowlarity[i]['caller']) ? knowlarity[i]['caller'] : '-',
+        knowlarity_number: this.utilService.isNonEmpty(knowlarity[i]['knowlarity_number']) ? knowlarity[i]['knowlarity_number'] : '-'
+      })
+      knowlaritysArray.push(knowlaritysInfo);
+    }
+ // console.log('knowlaritysArra
+   return knowlaritysArray;
   }
 
 }
