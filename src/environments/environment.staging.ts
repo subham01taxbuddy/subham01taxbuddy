962320d1676182255dd41097bf6c1e2b2ac13f87
Ashish Hulwan <ashish.hulwan@ssbainnovations.com>
2021-08-26 02:46:38 +0530

    Removed unwanted dependencies

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,22 +1,3 @@
-/**
- * (c) OneGreenDiary Software Pvt. Ltd. 
- * This file is a part of OneGreenDiary platform code base.
- *
- * This file is distributed under following terms:
- * 1) OneGreenDiary owns the OneGreenDiary platform, of which this file is a part.
- * 2) Any modifications to the base platform by OneGreenDiary is owned by OneGreenDiary and will be 
- *    non-exclusively used by OneGreenDiary Software Pvt. Ltd. for its clients and partners.
- * 3) Rights of any third-party customizations that do not alter the base platform, 
- *    solely reside with the third-party.  
- * 4) OneGreenDiary Software Pvt. Ltd. is free to  change the licences of the base platform to permissive 
- *    opensource licences (e.g. Apache/EPL/MIT/BSD) in future.
- * 5) Onces OneGreenDiary platform is delivered to third party, they are free to modify the code for their internal use.
- *    Any such modifications will be solely owned by the third party.
- * 6) The third party may not redistribute the OneGreenDiary platform code base in any form without 
- *    prior agreement with OneGreenDiary Software Pvt. Ltd. 
- * 7) Third party agrees to preserve the above notice for all the OneGreenDiary platform files.
- */
-
 export const environment = {
 	production: false,
 	url: "https://uat-api.taxbuddy.com",
@@ -44,7 +25,7 @@ export const environment = {
 		federationTarget: 'COGNITO_IDENTITY_POOLS',
 		authenticationFlowType: 'USER_PASSWORD_AUTH'
 	},
-	botIds: ["vaibhav.gaikwad@ssbainnovations.com", "ashish.hulwan@ssbainnovations.com", "dev_kommunicate@ssbainnovations.com", "ashish-upylx", "kommunicate-dev-cgvmc",
+	/* botIds: ["vaibhav.gaikwad@ssbainnovations.com", "ashish.hulwan@ssbainnovations.com", "dev_kommunicate@ssbainnovations.com", "ashish-upylx", "kommunicate-dev-cgvmc",
 	"txbdycustomchat-kjbxd", "vaibhav-zpyuj", "welcome-6mb0j"],
 
 	agentId: [ 	{"label" : "ashish", "value" : "ashish.hulwan@ssbainnovations.com"},
@@ -56,5 +37,5 @@ export const environment = {
 				{"label" : "uat.agent1", "value" : "uat.agent1@taxbuddy.com"},
 				{"label" : "uatagent2", "value" : "uatagent2@taxbuddy.com"},
 				{"label" : "uatagent3", "value" : "uatagent3@taxbuddy.com"}
-			]
+			] */
 };
