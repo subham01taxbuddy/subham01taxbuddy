b02e4ef4364588e21bec13b320c615113452fc5c
Ashish Hulwan <ashish.hulwan@ssbainnovations.com>
2021-08-25 10:44:29 +0530

    ITR filed status added in manual filing update

--- src/app/pages/itr-filing/update-manual-filing/update-manual-filing.component.ts
+++ src/app/pages/itr-filing/update-manual-filing/update-manual-filing.component.ts
@@ -5,6 +5,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { ItrMsService } from 'app/services/itr-ms.service';
+import { UserMsService } from 'app/services/user-ms.service';
 
 @Component({
   selector: 'app-update-manual-filing',
@@ -19,6 +20,7 @@ export class UpdateManualFilingComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private itrMsService: ItrMsService,
+    private userMsService: UserMsService,
     public location: Location,
     public utilsService: UtilsService) {
   }
@@ -36,6 +38,7 @@ export class UpdateManualFilingComponent implements OnInit {
       const param = `${ApiEndpoints.itrMs.itrManuallyData}`
       this.itrMsService.putMethod(param, this.data).subscribe((res: any) => {
         console.log(res);
+        this.updateStatus();
         this.loading = false;
         this.utilsService.showSnackBar('Manual Filing Details updated successfully')
         this.location.back();
@@ -44,6 +47,34 @@ export class UpdateManualFilingComponent implements OnInit {
         this.loading = false;
       })
     }
+  }
+
+  async updateStatus() {
+    const fyList = await this.utilsService.getStoredFyList();
+    const currentFyDetails = fyList.filter(item => item.isFilingActive);
+    if (!(currentFyDetails instanceof Array && currentFyDetails.length > 0)) {
+      // this.utilsService.showSnackBar('There is no any active filing year available')
+      return;
+    }
+    const param = '/itr-status'
+    const request = {
+      "statusId": 11, // ITR FILED
+      "userId": this.data.userId,
+      "assessmentYear": currentFyDetails[0].assessmentYear,
+      "completed": true,
+      "serviceType": "ITR"
+    }
+
+    // this.loading = true;
+    this.userMsService.postMethod(param, request).subscribe(result => {
+      console.log('##########################', result['statusId']);
+      // this.utilsService.showSnackBar('Filing status updated successfully.')
+      // this.sendValue.emit(result['statusId']);
+      // this.loading = false;
+    }, err => {
+      // this.loading = false;
+      // this.utilsService.showSnackBar('Failed to update Filing status.')
+    })
   }
 }
 
