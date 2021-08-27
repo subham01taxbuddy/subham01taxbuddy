6235e31fbafb056001374a850082990605b182b9
BhondaveSagarSharad <sagar@ssbainnovations.com>
2021-08-27 13:22:03 +0530

    knowlarity reports part added

---
+++ src/app/pages/main-reports/main-reports-routing.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ItrFillingReportComponent } from "./itr-filling-report/itr-filling-report.component";
+import { KnowlarityReportComponent } from "./knowlarity-report/knowlarity-report.component";
+import { RepoByAgentNameComponent } from "./knowlarity-report/repo-by-agent-name/repo-by-agent-name.component";
+import { RepoBySmeNameComponent } from "./knowlarity-report/repo-by-sme-name/repo-by-sme-name.component";
+import { MainReportsComponent } from "./main-reports/main-reports.component";
+
+const routes :Routes = [
+    {path: '', component: MainReportsComponent,
+     children:[
+        {path: 'knowlarity-repo', component: KnowlarityReportComponent,
+        children: [
+            {path: 'sme-wise', component: RepoBySmeNameComponent},
+            {path: 'agent-wise', component: RepoByAgentNameComponent },
+            {path: '', redirectTo: 'sme-wise', pathMatch: ''}
+        ]},
+        {path: 'itr-filing', component: ItrFillingReportComponent,
+        children: [
+           
+        ]},
+        {path: '', redirectTo: 'knowlarity-repo', pathMatch: ''}
+     ]
+    }
+] 
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class MainReportsRoutingModule {}
---
+++ src/app/pages/main-reports/main-reports.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "app/shared/shared.module";
+import { NgxLoadingModule } from "ngx-loading";
+import { MainReportsRoutingModule } from "./main-reports-routing";
+import { MainReportsComponent } from './main-reports/main-reports.component';
+import { KnowlarityReportComponent } from './knowlarity-report/knowlarity-report.component';
+import { RepoBySmeNameComponent } from './knowlarity-report/repo-by-sme-name/repo-by-sme-name.component';
+import { RepoByAgentNameComponent } from './knowlarity-report/repo-by-agent-name/repo-by-agent-name.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ItrFillingReportComponent } from './itr-filling-report/itr-filling-report.component';
+
+@NgModule({
+    declarations: [MainReportsComponent, KnowlarityReportComponent, RepoBySmeNameComponent, RepoByAgentNameComponent, ItrFillingReportComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MainReportsRoutingModule,
+        SharedModule,
+        NgxLoadingModule.forRoot({}),
+    ]
+})
+
+export class MainReportsModule {}
