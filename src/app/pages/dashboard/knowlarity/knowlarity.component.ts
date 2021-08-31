666b523ad9418e55e9804fe371c87155c8e93c76
BhondaveSagarSharad <sagar@ssbainnovations.com>
2021-08-31 17:36:00 +0530

    revert knowlarity new changes

--- src/app/pages/dashboard/knowlarity/knowlarity.component.ts
+++ src/app/pages/dashboard/knowlarity/knowlarity.component.ts
@@ -16,34 +16,35 @@ export class KnowlarityComponent implements OnInit {
   knowlarityData: any= [];
 
   constructor(private utilService: UtilsService,  @Inject(LOCALE_ID) private locale: string, private route: Router, private ngZone: NgZone) {
-    const knowlarityScript = document.createElement('script');
-    knowlarityScript.innerHTML = `var URL = "https://konnectprodstream3.knowlarity.com:8200/update-stream/560397a2-d875-478b-8003-cc4675e9a0eb/konnect"
-                                  var knowlarityData = [];
-                                  var aa = 0;
-                                  source = new EventSource(URL);
-                                  source.onmessage = function (event) {
-                                  var data = JSON.parse(event.data)
-                                  console.log('Received an event .......');
-                                  console.log(data);
-                                  knowlarityData.push(data)
-                                  window.angularComponentReference.zone.run(() => { window.angularComponentReference.loadKnowlarityData(data); });  
+    // const knowlarityScript = document.createElement('script');
+    // knowlarityScript.innerHTML = `var URL = "https://konnectprodstream3.knowlarity.com:8200/update-stream/560397a2-d875-478b-8003-cc4675e9a0eb/konnect"
+    //                               var knowlarityData = [];
+    //                               var aa = 0;
+    //                               source = new EventSource(URL);
+    //                               source.onmessage = function (event) {
+    //                               var data = JSON.parse(event.data)
+    //                               console.log('Received an event .......');
+    //                               console.log(data);
+    //                               knowlarityData.push(data)
+    //                               window.angularComponentReference.zone.run(() => { window.angularComponentReference.loadKnowlarityData(data); });  
                                 
-                             }`
-    knowlarityScript.id = '_webengage_script_tag';
-    knowlarityScript.type = 'text/javascript';
-    document.head.appendChild(knowlarityScript);
+    //                          }`
+    // knowlarityScript.id = '_webengage_script_tag';
+    // knowlarityScript.type = 'text/javascript';
+    // document.head.appendChild(knowlarityScript);
    }
 
   ngOnInit() {
-     window['angularComponentReference'] = { component: this, zone: this.ngZone, loadKnowlarityData: (res) => {
-       console.log('res: ',res)
-       this.storeData(res)
-      } };  
-    // this.getKnowlarityInfo();
+    //  window['angularComponentReference'] = { component: this, zone: this.ngZone, loadKnowlarityData: (res) => {
+    //    console.log('res: ',res)
+    //    this.storeData(res)
+    //   } };  
 
-    // setInterval(()=>{
-    //   this.getKnowlarityInfo();
-    // }, 1000);
+    this.getKnowlarityInfo();
+
+    setInterval(()=>{
+      this.getKnowlarityInfo();
+    }, 1000);
   }
 
   storeData(knowData){
@@ -51,22 +52,22 @@ export class KnowlarityComponent implements OnInit {
     this.knowlarityData = this.knowlarityData.reverse();
   }
 
-  // getKnowlarityInfo(){
-  //   let knowlarityInfo = JSON.parse(localStorage.getItem('INBOND_KNOWLARITY'));
-  //   this.knowlarityData = [];
-  //   var knowlarityArray = [];
-  //   if(knowlarityInfo instanceof Array && knowlarityInfo.length > 0){
-  //     knowlarityArray = knowlarityInfo.reverse();
-  //     if(knowlarityArray.length >= 50){
-  //       for(let i=0; i< 50; i++){
-  //         this.knowlarityData.splice(i, 0, knowlarityArray[i]);
-  //       }
-  //     }
-  //     else{
-  //       this.knowlarityData = knowlarityArray;
-  //     }
-  //   }
-  // }
+  getKnowlarityInfo(){
+    let knowlarityInfo = JSON.parse(localStorage.getItem('INBOND_KNOWLARITY'));
+    this.knowlarityData = [];
+    var knowlarityArray = [];
+    if(knowlarityInfo instanceof Array && knowlarityInfo.length > 0){
+      knowlarityArray = knowlarityInfo.reverse();
+      if(knowlarityArray.length >= 50){
+        for(let i=0; i< 50; i++){
+          this.knowlarityData.splice(i, 0, knowlarityArray[i]);
+        }
+      }
+      else{
+        this.knowlarityData = knowlarityArray;
+      }
+    }
+  }
 
   showCustomerInfo(mobNo){
     console.log(typeof mobNo, mobNo.substring(3, mobNo.length));
