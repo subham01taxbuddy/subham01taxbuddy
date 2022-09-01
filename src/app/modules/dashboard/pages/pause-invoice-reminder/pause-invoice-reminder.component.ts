3d77be9108148fb970980f5fb78b5fa8db3495b9
Sagar <sagar.kelkar@ssbainnovations.com>
2022-09-01 16:25:41 +0530

    1. Changed the name from to 'Engagement' to 'Others' in the sidebar. 2. Created a new component 'pause-invoice-reminder' 3. Added a new tab 'Pause Reminders' besides 'Knowlarity' in the dashboard component. 4. Added a search option to search for invoice numbers under the new tab 'Pause Reminders'.

---
+++ src/app/modules/dashboard/pages/pause-invoice-reminder/pause-invoice-reminder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pause-invoice-reminder',
+  templateUrl: './pause-invoice-reminder.component.html',
+  styleUrls: ['./pause-invoice-reminder.component.scss']
+})
+export class PauseInvoiceReminderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
