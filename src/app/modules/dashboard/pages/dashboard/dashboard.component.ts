ef48007f2bb1a87affdc413cbf0d6069bfb6f658
Ashwini Shahapurkar <ashwini.shahapurkar4@gmail.com>
2022-09-07 15:53:38 +0530

    Fixed TB2-1161
    (cherry picked from commit 67f9cbb88eaf5673b9daf33356c8f49d8dcb9a83)

--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoleBaseAuthGuardService } from 'src/app/modules/shared/services/role-base-auth-guard.service';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     // styleUrls: ['./app.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+    loggedInUserData: any;
+    constructor(private roleBaseAuthGuardService: RoleBaseAuthGuardService,) { }
+    ngOnInit() {
+        this.loggedInUserData = JSON.parse(localStorage.getItem("UMD")) || {};
+    }
 
-    constructor() {
-
+    isApplicable(permissionRoles) {
+        return this.roleBaseAuthGuardService.checkHasPermission(this.loggedInUserData.USER_ROLE, permissionRoles);
     }
 }
