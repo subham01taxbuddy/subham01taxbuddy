4926a949d2d1b870d9b626a2faebedd9637d9dce
Ashwini <ashwini@ssbainnovations.com>
2022-11-25 23:16:51 +0530

    Fixed  TB2-1606

--- src/app/pages/itr-filing/salary/salary.component.ts
+++ src/app/pages/itr-filing/salary/salary.component.ts
@@ -58,21 +58,9 @@ export class SalaryComponent implements OnInit {
     detailed: false
   }, {
     "id": null,
-    "seqNum": 3,
-    "value": "REMUNERATION_REC",
-    "label": "Renumeration received u/s 10(6)",
-    "detailed": false
-  }, {
-    "id": null,
     "seqNum": 4,
     "value": "CHILDREN_EDUCATION",
     "label": "Children education allowance",
-    "detailed": false
-  }, {
-    "id": null,
-    "seqNum": 5,
-    "value": "HOSTEL_EXPENDITURE",
-    "label": "Hostel expenditure allowance",
     "detailed": false
   }, {
     "id": null,
@@ -91,12 +79,6 @@ export class SalaryComponent implements OnInit {
     "seqNum": 8,
     "value": "LEAVE_ENCASHMENT",
     "label": "Leave Encashment u/s 10(10AA)",
-    "detailed": false
-  }, {
-    "id": null,
-    "seqNum": 9,
-    "value": "COMPENSATION_ON_VRS",
-    "label": "Voluntary Retirement Scheme u/s 10(10C)",
     "detailed": false
   }, {
     "id": null,
