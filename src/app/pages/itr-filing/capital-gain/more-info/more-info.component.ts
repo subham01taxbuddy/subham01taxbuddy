cc3b4e0ec50e3d15b2d318227b2fa39aac52b193
Sushama Aher <sushma.aher@ssbainnovations.com>
2022-11-30 15:24:46 +0530

    Added schedule CFL api integration

--- src/app/pages/itr-filing/capital-gain/more-info/more-info.component.ts
+++ src/app/pages/itr-filing/capital-gain/more-info/more-info.component.ts
@@ -4,6 +4,7 @@ import { GridOptions } from 'ag-grid-community';
 import { MatDialog } from '@angular/material/dialog'
 import { AppConstants } from 'src/app/modules/shared/constants';
 import { ITR_JSON } from 'src/app/modules/shared/interfaces/itr-input.interface';
+import { ItrMsService } from 'src/app/services/itr-ms.service';
 
 
 @Component({
@@ -13,18 +14,23 @@ import { ITR_JSON } from 'src/app/modules/shared/interfaces/itr-input.interface'
 })
 export class MoreInfoComponent implements OnInit {
   rowHeight: number;
-  constructor(
-    public utilsService: UtilsService,
-    public matDialog: MatDialog) {
-    this.rowHeight = 50;
-    this.ITR_JSON = JSON.parse(sessionStorage.getItem(AppConstants.ITR_JSON));
-    this.Copy_ITR_JSON = JSON.parse(JSON.stringify(this.ITR_JSON));
-    this.getTableData();
-  }
   loading = false;
   ITR_JSON: ITR_JSON;
   Copy_ITR_JSON: ITR_JSON;
+
   public scheduleCFLLoss: GridOptions;
+
+  constructor(
+    public utilsService: UtilsService,
+    public matDialog: MatDialog,
+    private itrMsService: ItrMsService,
+  ) {
+    this.rowHeight = 50;
+    this.ITR_JSON = JSON.parse(sessionStorage.getItem(AppConstants.ITR_JSON));
+    this.Copy_ITR_JSON = JSON.parse(JSON.stringify(this.ITR_JSON));
+
+    this.getTableData();
+  }
 
   ngOnInit() {
     this.utilsService.smoothScrollToTop();
@@ -38,7 +44,7 @@ export class MoreInfoComponent implements OnInit {
     return [
       {
         headerName: 'Assessment Year',
-        field: 'AssessmentYear',
+        field: 'assessmentPastYear',
         editable: false,
         suppressMovable: true,
         pinned: 'left',
@@ -46,14 +52,14 @@ export class MoreInfoComponent implements OnInit {
       },
       {
         headerName: 'Date of Filing',
-        field: 'DOF',
+        field: 'dateofFilling',
         editable: true,
         suppressMovable: true,
       },
 
       {
         headerName: 'House property Loss',
-        field: 'HPL',
+        field: 'housePropertyLoss',
         suppressMovable: true,
         editable: true,
       },
@@ -64,23 +70,23 @@ export class MoreInfoComponent implements OnInit {
         children: [
           {
             headerName: 'a. Brought Forward Business Loss',
-            field: 'FBL',
-            editable: false,
+            field: 'broughtForwordBusinessLoss',
+            editable: true,
             suppressMovable: true,
           },
           {
             headerName: 'b. Amount as adjusted on account of opting for taxation u/s 115BAC',
-            field: 'taxation',
-            editable: false,
+            field: 'adjustedAmount',
+            editable: true,
             width: 300,
             suppressMovable: true,
           },
 
           {
             headerName: 'c. Brought forward Business loss available for set off during the year [c=a-b]',
-            field: 'FBLDuringYear',
+            field: 'setOffDuringTheYear',
             suppressMovable: true,
-            editable: false,
+            editable: true,
             width: 300,
           }
         ],
@@ -88,20 +94,20 @@ export class MoreInfoComponent implements OnInit {
       },
       {
         headerName: 'Short Term Capital Loss',
-        field: 'STCL',
-        editable: false,
+        field: 'STCGLoss',
+        editable: true,
         suppressMovable: true,
       },
       {
         headerName: 'Long Term Capital Loss',
-        field: 'LTCL',
-        editable: false,
+        field: 'LTCGLoss',
+        editable: true,
         suppressMovable: true,
       },
       {
         headerName: 'Loss from Speculative Business',
-        field: 'LFSB',
-        editable: false,
+        field: 'speculativeBusinessLoss',
+        editable: true,
         suppressMovable: true,
       },
 
@@ -143,18 +149,18 @@ export class MoreInfoComponent implements OnInit {
 
   getRowData() {
     return [
-      { AssessmentYear: '2014-15', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2015-16', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2016-17', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2017-18', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2018-19', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2019-20', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2020-21', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2021-22', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: 'Total of earlier year losses', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: 'Adjustment of above losses in schedule BFLA', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: '2022-23 (Current Year Losses)', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
-      { AssessmentYear: 'Total Loss carried forward to future years', DOF: '', HPL: '', FBL: '', taxation: '', FBLDuringYear: '', STCL: '', LTCL: '', LFSB: '', Actions: '' },
+      { assessmentPastYear: '2014-15', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2015-16', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2016-17', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2017-18', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2018-19', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2019-20', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2020-21', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2021-22', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: 'Total of earlier year losses', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: 'Adjustment of above losses in schedule BFLA', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: '2022-23 (Current Year Losses)', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
+      { assessmentPastYear: 'Total Loss carried forward to future years', dateofFilling: '', housePropertyLoss: '', broughtForwordBusinessLoss: '', adjustedAmount: '', setOffDuringTheYear: '', STCGLoss: '', LTCGLoss: '', speculativeBusinessLoss: '', Actions: '' },
     ];
   }
 
@@ -180,5 +186,22 @@ export class MoreInfoComponent implements OnInit {
   }
 
   continue() {
+    this.Copy_ITR_JSON.carryForwordLosses = this.scheduleCFLLoss.rowData;
+    console.log(this.Copy_ITR_JSON)
+    this.loading = true;
+    const param = '/itr/' + this.ITR_JSON.userId + '/' + this.ITR_JSON.itrId + '/' + this.ITR_JSON.assessmentYear;
+    this.itrMsService.putMethod(param, this.Copy_ITR_JSON).subscribe((result: any) => {
+      this.ITR_JSON = result;
+      sessionStorage.setItem('ITR_JSON', JSON.stringify(this.ITR_JSON));
+      this.loading = false;
+      this.utilsService.showSnackBar('Schedule CFL updated successfully');
+      console.log('Assets & Liabilities save result=', result);
+      this.utilsService.smoothScrollToTop();
+    }, error => {
+      this.Copy_ITR_JSON = JSON.parse(JSON.stringify(this.ITR_JSON));
+      this.loading = false;
+      this.utilsService.showSnackBar('Failed to add schedule CFL, please try again.');
+      this.utilsService.smoothScrollToTop();
+    });
   }
 }
