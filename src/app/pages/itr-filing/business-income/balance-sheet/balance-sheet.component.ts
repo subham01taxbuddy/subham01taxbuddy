3d6ad36e1f367b900f87aff77d46fc76dbb2328c
Ashwini <ashwini@ssbainnovations.com>
2022-12-29 11:20:06 +0530

    added itr-type api call before ITR object is saved

--- src/app/pages/itr-filing/business-income/balance-sheet/balance-sheet.component.ts
+++ src/app/pages/itr-filing/business-income/balance-sheet/balance-sheet.component.ts
@@ -308,8 +308,7 @@ export class BalanceSheetComponent implements OnInit {
     this.Copy_ITR_JSON.business.fixedAssetsDetails = this.depreciationObj;
 
     console.log(this.Copy_ITR_JSON);
-    const param = '/itr/' + this.ITR_JSON.userId + '/' + this.ITR_JSON.itrId + '/' + this.ITR_JSON.assessmentYear;
-    this.itrMsService.putMethod(param, this.Copy_ITR_JSON).subscribe((result: any) => {
+    this.utilsService.saveItrObject(this.Copy_ITR_JSON).subscribe((result: any) => {
       this.loading = false;
       this.ITR_JSON = result;
       sessionStorage.setItem('ITR_JSON', JSON.stringify(this.ITR_JSON));
