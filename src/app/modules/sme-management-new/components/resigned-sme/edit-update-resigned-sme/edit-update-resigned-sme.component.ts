78cd7fbf3fa6486fb907f8d85f4ef7fe18a6b0c2
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-03-24 11:47:51 +0530

    Added Missing fields in Req Body of Sme Management

--- src/app/modules/sme-management-new/components/resigned-sme/edit-update-resigned-sme/edit-update-resigned-sme.component.ts
+++ src/app/modules/sme-management-new/components/resigned-sme/edit-update-resigned-sme/edit-update-resigned-sme.component.ts
@@ -259,7 +259,7 @@ export class EditUpdateResignedSmeComponent implements OnInit {
     name: new FormControl("",[Validators.required]),
     email: new FormControl(''),
     languages: new FormControl(''),
-    referredPerson: new FormControl(''),
+    referredBy: new FormControl(''),
     itrTypes: new FormControl(''),
     qualification: new FormControl(''),
     state: new FormControl(''),
@@ -279,8 +279,8 @@ export class EditUpdateResignedSmeComponent implements OnInit {
   get languages(){
     return this.smeFormGroup.controls['languages'] as FormControl
   }
-  get referredPerson(){
-    return this.smeFormGroup.controls['referredPerson'] as FormControl
+  get referredBy(){
+    return this.smeFormGroup.controls['referredBy'] as FormControl
   }
   get itrTypes(){
     return this.smeFormGroup.controls['itrTypes'] as FormControl
@@ -440,15 +440,21 @@ export class EditUpdateResignedSmeComponent implements OnInit {
         name: this.name.value,
         email: this.email.value,
         mobileNumber: this.mobileNumber.value,
-        callingNumber: this.smeObj.callingNumber,
+        referredBy:this.referredBy.value,
+        qualification:this.qualification.value,
+        state:this.state.value,
+        callingNumber: this.callingNumber.value,
         serviceType: this.smeObj.serviceType,
         roles: this.smeObj.roles,
         languages:this.languages.value,
         parentId: this.smeObj.parentId,
         botId: this.smeObj.botId,
-        displayName: this.smeObj.displayName,
+        displayName: this.displayName.value,
         active: this.smeObj.active,
+        leaveStartDate:this.leaveStartDate.value,
+        leaveEndDate:this.leaveEndDate.value,
         joiningDate: this.joiningDate.value,
+        resigningDate:this.resigningDate.value,
         internal: this.internal.value == 'internal'? true :false,
         assignmentStart: this.smeObj.assignmentStart,
         itrTypes: this.itrTypes.value,
@@ -541,5 +547,7 @@ export interface SmeObj {
   callingNumberList: string[]
   filerCallingNumberHistory: any
   owner: boolean
-
+  referredBy:string
+  qualification:string
+  state:string
 }
