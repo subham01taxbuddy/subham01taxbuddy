963a4a412c99ca3c0940b90649ef967b19622a62
shrikanthramesh <shrikant.eligeti@taxbuddy.com>
2023-04-14 13:54:44 +0530

    Customer Profile

--- src/app/modules/itr-filing/itr-wizard/pages/prefill-id/prefill-id.component.ts
+++ src/app/modules/itr-filing/itr-wizard/pages/prefill-id/prefill-id.component.ts
@@ -26,6 +26,7 @@ export class PrefillIdComponent implements OnInit {
   loading = false;
   showEriView = false;
   ITR_JSON: ITR_JSON;
+  ITR_Type: string;
   @Input() data: any;
   @Output() skipPrefill: EventEmitter<any> = new EventEmitter();
 
@@ -82,6 +83,7 @@ export class PrefillIdComponent implements OnInit {
     this.downloadPrefill = true;
   }
 
+  // PREFILL PAN VALIDATION
   uploadJsonFile(file: FileList) {
     console.log('File in prefill', file);
     if (file.length > 0) {
@@ -94,7 +96,7 @@ export class PrefillIdComponent implements OnInit {
         let JSONData = JSON.parse(jsonRes);
 
         let panNo = JSONData.personalInfo?.pan;
-        // let mobileNo = JSONData.personalInfo?.address?.mobileNo;
+        let mobileNo = JSONData.personalInfo?.address?.mobileNo;
         if (panNo !== this.data?.panNumber) {
           this.toastMessageService.alert(
             'error',
@@ -110,8 +112,135 @@ export class PrefillIdComponent implements OnInit {
     }
   }
 
-  upload() {
-    document.getElementById('input-jsonfile-id').click();
+  // Uploading Utility JSON
+  uploadUtilityItrJson(file: FileList) {
+    if (file.length > 0) {
+      this.uploadDoc = file.item(0);
+
+      //read the file to get details upload and validate
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        let jsonRes = e.target.result;
+        let JSONData = JSON.parse(jsonRes);
+        // console.log('JSONData: ', JSONData);
+        this.mapItrJson(JSONData.ITR);
+      };
+      reader.readAsText(this.uploadDoc);
+    }
+  }
+
+  ITR_Obj: ITR_JSON;
+  mapItrJson(ItrJSON: any) {
+    // ITR_Obj IS THE TB ITR OBJECT
+    this.ITR_Obj = JSON.parse(sessionStorage.getItem(AppConstants.ITR_JSON));
+    console.log('TB ITR OBJ - this.ITR_Obj: ', this.ITR_Obj);
+
+    // ITR JSON IS THE UPLOADED UTILITY JSON
+    console.log('Uploaded Utility: ', ItrJSON);
+
+    if (ItrJSON.hasOwnProperty('ITR1')) {
+      this.ITR_Obj.itrType = '1';
+      this.ITR_Type = 'ITR1';
+    } else if (ItrJSON.hasOwnProperty('ITR2')) {
+      this.ITR_Obj.itrType = '2';
+      this.ITR_Type = 'ITR2';
+    } else if (ItrJSON.hasOwnProperty('ITR3')) {
+      this.ITR_Obj.itrType = '3';
+      this.ITR_Type = 'ITR3';
+    } else if (ItrJSON.hasOwnProperty('ITR4')) {
+      this.ITR_Obj.itrType = '4';
+      this.ITR_Type = 'ITR4';
+    }
+
+    //Finding the way
+    console.log(ItrJSON[this.ITR_Type]);
+
+    // HAVE TO CREATE SEPERATE AS THE JSON STRUCTURE IS DIFFERENT FOR DIFFERENT ITR TYPES
+    if ((this.ITR_Type = 'ITR1')) {
+      // CUSTOMER PROFILE
+      this.ITR_Obj.panNumber = ItrJSON[this.ITR_Type].PersonalInfo.PAN;
+      this.ITR_Obj.contactNumber =
+        ItrJSON[this.ITR_Type].PersonalInfo.Address.MobileNo;
+      this.ITR_Obj.email =
+        ItrJSON[this.ITR_Type].PersonalInfo.Address.EmailAddress;
+      this.ITR_Obj.family[0].fName =
+        ItrJSON[this.ITR_Type].PersonalInfo.AssesseeName.FirstName;
+      this.ITR_Obj.family[0].lName =
+        ItrJSON[this.ITR_Type].PersonalInfo.AssesseeName.SurNameOrOrgName;
+      this.ITR_Obj.family[0].fatherName =
+        ItrJSON[this.ITR_Type].Verification.Declaration.FatherName;
+      // HAVE TO SET THE RES STATUS MANUALLY AS THIS KEY IS NOT AVAILABLE IN JSON AS OF 14/04/23 AND ONLY "RESIDENT" ARE ALLOWED UNDER ITR1 - PENDING
+      this.ITR_Obj.residentialStatus = 'Resident';
+
+      // HAVE TO CHECK WHAT IS THE VALUE THAT WE ARE TAKING FOR EMPLOYER CATEGORY AS THE KEY IN JSON MIGHT BE DIFFERENT - PENDING
+      this.ITR_Obj.employerCategory =
+        ItrJSON[this.ITR_Type].PersonalInfo.EmployerCategory;
+      // HAVE TO SET THE RETURN TYPE HERE - PENDING
+      // this.ITR_Obj.returnType =
+      //   ItrJSON[this.ITR_Type].FilingStatus.ReturnFileSec;
+      this.ITR_Obj.aadharNumber =
+        ItrJSON[this.ITR_Type].PersonalInfo.AadhaarCardNo;
+      this.ITR_Obj.family[0].dateOfBirth =
+        ItrJSON[this.ITR_Type].PersonalInfo.DOB;
+
+      sessionStorage.setItem(
+        AppConstants.ITR_JSON,
+        JSON.stringify(this.ITR_Obj)
+      );
+      console.log(this.ITR_Obj);
+    }
+
+    this.ITR_Obj.employerCategory = ItrJSON.PersonalInfo.EmployerCategory;
+    this.ITR_Obj.regime = ItrJSON.FilingStatus.NewTaxRegime;
+
+    let address = ItrJSON.PersonalInfo.Address;
+    this.ITR_Obj.email = address.EmailAddress;
+    this.ITR_Obj.contactNumber = address.MobileNo;
+    this.ITR_Obj.contactNumber = address.MobileNo;
+
+    let hole_address =
+      (address.hasOwnProperty('ResidenceNo') ? address.ResidenceNo : '') +
+      ' ,' +
+      (address.hasOwnProperty('ResidenceName') ? address.ResidenceName : '') +
+      ' ,' +
+      (address.hasOwnProperty('RoadOrStreet') ? address.RoadOrStreet : '') +
+      ' ,' +
+      (address.hasOwnProperty('LocalityOrArea') ? address.LocalityOrArea : '');
+    this.ITR_Obj.address.premisesName = hole_address;
+    this.ITR_Obj.address.pinCode = address.PinCode;
+
+    //country, city, state data getting through api call unsin pine code
+
+    var assessmentYear;
+    if (ItrJSON.hasOwnProperty('Form_ITR1')) {
+      assessmentYear = ItrJSON.Form_ITR1.AssessmentYear;
+    } else if (ItrJSON.hasOwnProperty('Form_ITR4')) {
+      assessmentYear = ItrJSON.Form_ITR4.AssessmentYear;
+    }
+
+    if (assessmentYear === '2023') {
+      this.ITR_Obj.assessmentYear = '2023-24';
+      this.ITR_Obj.financialYear = '2022-23';
+    } else if (assessmentYear === '2022') {
+      this.ITR_Obj.assessmentYear = '2022-23';
+      this.ITR_Obj.financialYear = '2021-22';
+    } else if (assessmentYear === '2021') {
+      this.ITR_Obj.assessmentYear = '2021-22';
+      this.ITR_Obj.financialYear = '2020-21';
+    } else if (assessmentYear === '2020') {
+      this.ITR_Obj.assessmentYear = '2020-21';
+      this.ITR_Obj.financialYear = '2019-20';
+    }
+
+    sessionStorage.setItem(AppConstants.ITR_JSON, JSON.stringify(this.ITR_Obj));
+  }
+
+  upload(type: string) {
+    if (type == 'pre-filled') {
+      document.getElementById('input-jsonfile-id').click();
+    } else if (type == 'utility') {
+      document.getElementById('input-utility-file-jsonfile-id').click();
+    }
   }
 
   uploadPrefillJson() {
