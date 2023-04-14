ab0eba1478c461e968685c94d2d8818f79640cfa
Ashwini <ashwini@ssbainnovations.com>
2023-04-14 10:15:18 +0530

    Added mobile number and email filter

--- src/app/modules/subscription/components/tax-invoice/tax-invoice.component.ts
+++ src/app/modules/subscription/components/tax-invoice/tax-invoice.component.ts
@@ -223,6 +223,8 @@ export class TaxInvoiceComponent implements OnInit {
     status: new FormControl('Paid'),
     searchFiler: new FormControl(''),
     searchOwner: new FormControl(''),
+    mobile: new FormControl(''),
+    email: new FormControl(''),
   });
   get assessmentYear() {
     return this.invoiceFormGroup.controls['assessmentYear'] as FormControl;
@@ -241,6 +243,14 @@ export class TaxInvoiceComponent implements OnInit {
   }
   get searchOwner() {
     return this.invoiceFormGroup.controls['searchOwner'] as FormControl;
+  }
+
+  get mobile() {
+    return this.invoiceFormGroup.controls['mobile'] as FormControl;
+  }
+
+  get email() {
+    return this.invoiceFormGroup.controls['email'] as FormControl;
   }
 
   getOwner() {
@@ -326,7 +336,15 @@ export class TaxInvoiceComponent implements OnInit {
     if (this.filerDetails?.userId) {
       userFilter += `&filerUserId=${this.filerDetails.userId}`;
     }
-    param = `/v1/invoice/back-office?fromDate=${fromData}&toDate=${toData}&${data}${userFilter}${statusFilter}`;
+    let mobileFilter = '';
+    if(this.utilService.isNonEmpty(this.invoiceFormGroup.controls['mobile'].value) && this.invoiceFormGroup.controls['mobile'].valid){
+      mobileFilter = '&mobile=' + this.invoiceFormGroup.controls['mobile'].value;
+    }
+    let emailFilter = '';
+    if(this.utilService.isNonEmpty(this.invoiceFormGroup.controls['email'].value) && this.invoiceFormGroup.controls['email'].valid){
+      emailFilter = '&email=' + this.invoiceFormGroup.controls['email'].value;
+    }
+    param = `/v1/invoice/back-office?fromDate=${fromData}&toDate=${toData}&${data}${userFilter}${statusFilter}${mobileFilter}${emailFilter}`;
     this.itrService.getMethod(param).subscribe((response: any) => {
       this.loading = false;
       if(response.success) {
