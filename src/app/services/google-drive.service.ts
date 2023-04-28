468d6e1a21adf95459c163df5dc6401385a4cd8b
Ashwini <ashwini@ssbainnovations.com>
2023-04-28 17:08:49 +0530

    Google drive integration complete

--- src/app/services/google-drive.service.ts
+++ src/app/services/google-drive.service.ts
@@ -70,14 +70,7 @@ export class GoogleDriveService{
   }
 
   getDriveFiles(fileName, fileData, dataType){
-    // showStatus("Loading Google Drive files...");
-    // gapi.client.init({
-    //   apiKey: driveApiKey,
-    //   clientId: driveClientId,
-    //   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-    //   scope: 'https://www.googleapis.com/auth/drive'
-    // });//.then(this.getDriveFiles);
-    // gapi.client.load('drive', 'v3', this.createExcelOnDrive2);
+
     gapi.client.load('drive', 'v3', ()=>{
       const driveId = '0AK1-Qhm_r1YrUk9PVA';
 
@@ -91,87 +84,39 @@ export class GoogleDriveService{
       var folderId = "1DlJN6xgPyrX_ijF7k6q4M_1rwiRFJkGt";
       var file = new Blob(fileData, {type: dataType});
 
-      var form = new FormData();
-      form.append('metadata', new Blob([JSON.stringify(fileMetadata)], { type: 'application/json' }));
-      form.append('file', file);
+      //this is gapi client way.. excel is created with data
+      gapi.client.request({
+        method:'POST',
+        path: `https://www.googleapis.com/drive/v3/files?uploadType=media&supportsAllDrives=true&driveId=${driveId}`,
+        'headers': {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + gapi.client.getToken()
+        },
+        body:{
+          name: fileName,
+          mimeType: 'application/vnd.google-apps.spreadsheet',
+          parents: folderId ? [folderId] : [],
+          metadata: fileMetadata,
+          file: file
+        }
+      }).execute((response)=>{
+        console.log('create response', response);
+        let fileId = response.id;
+        let uploadUrl = `https://www.googleapis.com/upload/drive/v3/files/${fileId}?supportsAllDrives=true`;
 
-      // gapi.client.request({
-      //   method:'POST',
-      //   path: `https://www.googleapis.com/drive/v3/files?uploadType=media&supportsAllDrives=true&driveId=${driveId}`,
-      //   'headers': {
-      //     'Content-Type': 'application/json',
-      //     'Authorization': 'Bearer ' + gapi.client.getToken()
-      //   },
-      //   body:{
-      //     name: fileName,
-      //     mimeType: 'application/vnd.google-apps.spreadsheet',
-      //     parents: folderId ? [folderId] : [],
-      //     metadata: fileMetadata,
-      //     file: file
-      //   }
-      //   // body: form
-      // }).execute((response)=>{
-      //   console.log('create response', response);
-      // });
+        var metadataOptions = new HttpHeaders();
+        metadataOptions = metadataOptions.append('Authorization', 'Bearer ' + gapi.client.getToken());
+        metadataOptions = metadataOptions.append('Content-Type', 'application/json');
+        metadataOptions = metadataOptions.append('X-Upload-Content-Length', file.size.toString());
+        metadataOptions = metadataOptions.append('X-Upload-Content-Type', 'application/octet-stream');
 
-      const contentType = dataType || 'application/octet-stream';
-      const baseRoot = gapi['config'].get('googleapis.config').root;
-      const authHeader = 'Bearer ' + gapi.client.getToken();//`Bearer ${gapi.client.getToken()}`;
-
-      const metadataHeaders = {
-        'Authorization': authHeader,
-        'Content-Type': 'application/json',
-        'X-Upload-Content-Length': file.size,
-        'X-Upload-Content-Type': contentType
-      };
-      // const metadataOptions = new RequestOptions({ headers: new Headers(metadataHeaders) });
-      var metadataOptions = new HttpHeaders();
-      metadataOptions = metadataOptions.append('Authorization', authHeader);
-      metadataOptions = metadataOptions.append('Content-Type', 'application/json');
-      metadataOptions = metadataOptions.append('X-Upload-Content-Length', file.size.toString());
-      metadataOptions = metadataOptions.append('X-Upload-Content-Type', contentType);
-
-      const url = `${baseRoot}/upload/drive/v3/files?uploadType=resumable&supportsAllDrives=true&driveId=${driveId}`;
-
-      const metadata = {
-        'name': fileName,
-        'mimeType': contentType,
-        'Content-Type': contentType,
-        'Content-Length': fileData.length
-      };
-
-      this.http.post(url, metadata, { headers : metadataOptions }).subscribe((metadataRes: any) => {
-
-        const locationUrl = `https://drive.google.com/file/d/${metadataRes.id}`//metadataRes.headers.get('Location');
-
-        const uploadHeaders = new HttpHeaders();
-        uploadHeaders.append('Content-Type', contentType);
-        uploadHeaders.append('X-Upload-Content-Type', contentType);
-        uploadHeaders.append('Authorization', authHeader);
-
-        // const uploadOptions = new RequestOptions({ headers: new Headers(uploadHeaders) });
-
-        this.http.put(locationUrl, file, {headers: uploadHeaders}).subscribe((uploadRes: any) => {
-          console.log(uploadRes.json());
+        this.http.patch(uploadUrl, file, { headers : metadataOptions }).subscribe((metadataRes: any) => {
+          console.log('upload by http', metadataRes);
+          window.open(`https://docs.google.com/spreadsheets/d/${metadataRes.id}`);
         });
-      }, (err) => {
-        console.error(err);
+
       });
-          /*return gapi.client.drive.files.create({
-            resource: fileMetadata,
-            requestBody: {
-              name: 'Zerodha.xlsx',
-              mimeType: 'application/vnd.google-apps.spreadsheet',
-              parents: folderId ? [folderId] : [],
-            },
-            media: {
-              mimeType: 'application/vnd.google-apps.spreadsheet',
-              //TODO: add correct stream data
-              // body:got.default.stream(fileUrl),
-            },
-            supportsAllDrives: true,
-            driveId: driveId,
-          });*/
+
     });
   }
 
@@ -231,70 +176,5 @@ export class GoogleDriveService{
       supportsAllDrives: true,
       driveId: driveId,
     });
-  }
-
-  async createExcelOnDrive2() {
-    // const googleDriveService = new GoogleDriveService();
-
-    const folderName = 'Parser';
-    var folder;
-
-    gapi.client.drive.files.list(
-      {
-        q: ` mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
-        // fields: 'files(id, name)',
-        // includeItemsFromAllDrives: true,
-        supportsAllDrives: true,
-        // driveId: driveId,
-      }).then(response => {
-        // if (err) {
-        //   console.log(err);
-        // }
-
-        console.log(response)
-        folder = (response.data.files ? response.data.files[0] : null);
-
-        const fileMetadata = {
-          name: 'Zerodha.xlsx',
-          mimeType: 'application/vnd.google-apps.spreadsheet',
-        };
-
-        const driveId = '0AK1-Qhm_r1YrUk9PVA';
-        var folderId = folder.od;
-        return gapi.client.drive.files.create({
-          resource: fileMetadata,
-          requestBody: {
-            name: 'Zerodha.xlsx',
-            mimeType: 'application/vnd.google-apps.spreadsheet',
-            parents: folderId ? [folderId] : [],
-          },
-          media: {
-            mimeType: 'application/vnd.google-apps.spreadsheet',
-            //TODO: add correct stream data
-            // body:got.default.stream(fileUrl),
-          },
-          supportsAllDrives: true,
-          driveId: driveId,
-        });
-      },
-    );
-
-    // console.log('creating folder');
-    // if (!folder) {
-    //   folder = await this.createFolder(folderName);
-    // }
-
-    /*console.log('remote file:', this.fileUrl);
-
-    console.log('saving to drive');
-    let createdFile = await this.saveExcel('Zerodha.xlsx', this.fileUrl, 'application/vnd.google-apps.spreadsheet', folder.id).catch((error) => {
-      console.error(error);
-    });
-
-    let redirectUrl = 'https://docs.google.com/spreadsheets/d/' + createdFile.data.id;
-    console.info('File uploaded successfully!', createdFile);
-    console.info('File available at', redirectUrl);
-
-    window.open(redirectUrl);*/
   }
 }
