6ba99117dd168ab6fe261b7a1a6eca1c3391648d
shrikanthramesh <shrikant.eligeti@taxbuddy.com>
2023-05-17 19:24:03 +0530

    resetEnv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,21 @@
 export const environment = {
-  production: true,
+  production: false,
   url_dev: 'https://dev-api.taxbuddy.com',
-  portal_url: 'https://gst-admin.taxbuddy.com/',
-  url: 'https://api.taxbuddy.com',
+  portal_url: 'https://uat-admin.taxbuddy.com/',
+  url: 'https://uat-api.taxbuddy.com',
   eri_url: 'https://api.taxbuddy.com',
   gst_gov_url: 'https://devapi.gst.gov.in',
   ifsc_url: 'https://ifsc.razorpay.com',
   amazonaws_url: 'https://9buh2b9cgl.execute-api.ap-south-1.amazonaws.com',
   reviewUrl: 'https://9buh2b9cgl.execute-api.ap-south-1.amazonaws.com/prod/',
-  isValidItr: true,
+  isValidItr: false,
   assistedKmScript: '../assets/assisted-km-script-uat.html',
-  environment: 'PROD',
-  admin_id: 1067,
-  webportal_url: 'https://itr.taxbuddy.com',
+  environment: 'UAT',
+  admin_id: 3000,
+  webportal_url: 'https://uat-itr.taxbuddy.com',
   lifecycleUrl:
     'https://ngd74g554pp72qp5ur3b55cvia0vfwur.lambda-url.ap-south-1.on.aws/itr/lifecycle-status',
-  lifecycleEnv: 'prod',
+  lifecycleEnv: 'qa',
 
   gdrive: {
     GOOGLE_DRIVE_CLIENT_ID:
@@ -25,13 +25,24 @@ export const environment = {
     // GOOGLE_DRIVE_API_KEY : 'AIzaSyDbc9tRF_zEqgITEen8BAIpbFeWF5i-w90',
     GOOGLE_DRIVE_API_KEY: 'AIzaSyDbc9tRF_zEqgITEen8BAIpbFeWF5i-w90',
     // GOOGLE_DRIVE_REFRESH_TOKEN : '1//04pkPTYbdma87CgYIARAAGAQSNwF-L9IrPQ3eZitgESUDwy99aGnxQxLEXLjZy0hJ2oVY0U6jyzt-G1-QwfmgQhP2-5B07tGiWGA'
-    FOLDER_ID: '15MjvRj7e18aqvkQUrFnEFAKSohUdVQBr',
+    FOLDER_ID: '1DlJN6xgPyrX_ijF7k6q4M_1rwiRFJkGt',
+  },
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyCJ2slZlHWDCsZLJs59NGU-lTAHOfucZyk',
+    authDomain: 'taxbuddy-uat.firebaseapp.com',
+    databaseURL: 'https://taxbuddy-uat.firebaseio.com',
+    projectId: 'taxbuddy-uat',
+    storageBucket: 'taxbuddy-uat.appspot.com',
+    messagingSenderId: '805239680508',
+    appId: '1:805239680508:web:b9258ec04023b0fda05679',
+    measurementId: 'G-DMWVKK98X2',
   },
 
   s3_cred: {
     // user_name: "ashish@ssbainnovations.com",
     // password: "testPwd8761$",
-    bucket: 'tb-gst-docs-dev',
+    bucket: 'tb-gst-docs-uat-dev',
     region: 'ap-south-1',
   },
   /* aws_cred: {
@@ -43,24 +54,14 @@ export const environment = {
   AMPLIFY_CONFIG: {
     aws_project_region: 'ap-south-1',
     aws_cognito_identity_pool_id:
-      'ap-south-1:441cb3f2-c1a9-4ca1-8964-ea56f5184aea',
+      'ap-south-1:f7280802-a172-4b2a-a3b2-adf642b67b38',
     aws_cognito_region: 'ap-south-1',
-    aws_user_pools_id: 'ap-south-1_s42HgYzaY',
-    aws_user_pools_web_client_id: '289f0frh4oj9rp5rh6q9opo4qn',
+    aws_user_pools_id: 'ap-south-1_7GHESh9rb',
+    aws_user_pools_web_client_id: 'pdtpeogn8a2viimond1u0debi',
     oauth: {},
     federationTarget: 'COGNITO_IDENTITY_POOLS',
     authenticationFlowType: 'USER_PASSWORD_AUTH',
   },
 
   externalScripts: {},
-  firebaseConfig: {
-    apiKey: 'AIzaSyCTINXADOkr4isp-GJXu5IjV8kPlqb7OZo',
-    authDomain: 'taxbuddy-29fe2.firebaseapp.com',
-    databaseURL: 'https://taxbuddy-29fe2.firebaseio.com',
-    projectId: 'taxbuddy-29fe2',
-    storageBucket: 'taxbuddy-29fe2.appspot.com',
-    messagingSenderId: '294419985055',
-    appId: '1:294419985055:web:ba4a7ae5511923269d28aa',
-    measurementId: 'G-ED3KSXPPS8',
-  },
 };
