a38c513618c550f22d15e029dd2c5f8c20fbac1e
Ashwini <ashwini@ssbainnovations.com>
2023-05-17 10:44:38 +0530

    Fixed : correct prod environment config for push notifications

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -52,14 +52,14 @@ export const environment = {
 	externalScripts: {
 	}
 	,
-	firebaseConfig : {
-		apiKey: "AIzaSyCTINXADOkr4isp-GJXu5IjV8kPlqb7OZo",
-		authDomain: "taxbuddy-29fe2.firebaseapp.com",
-		databaseURL: "https://taxbuddy-29fe2.firebaseio.com",
-		projectId: "taxbuddy-29fe2",
-		storageBucket: "taxbuddy-29fe2.appspot.com",
-		messagingSenderId: "294419985055",
-		appId: "1:294419985055:web:333066a5ba29aad79d28aa",
-		measurementId: "G-K3L7DS9C92"
-	  },
+  firebaseConfig: {
+    apiKey: "AIzaSyCTINXADOkr4isp-GJXu5IjV8kPlqb7OZo",
+    authDomain: "taxbuddy-29fe2.firebaseapp.com",
+    databaseURL: "https://taxbuddy-29fe2.firebaseio.com",
+    projectId: "taxbuddy-29fe2",
+    storageBucket: "taxbuddy-29fe2.appspot.com",
+    messagingSenderId: "294419985055",
+    appId: "1:294419985055:web:ba4a7ae5511923269d28aa",
+    measurementId: "G-ED3KSXPPS8"
+  }
 };
