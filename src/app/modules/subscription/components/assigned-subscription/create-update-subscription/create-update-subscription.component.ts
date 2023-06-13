1663a37ee5cbf75f9ba82804f0902e9184e3f7ac
Ashwini <ashwini@ssbainnovations.com>
2023-06-13 13:39:41 +0530

    Fixed: service type for create subscription in case of ITR U

--- src/app/modules/subscription/components/assigned-subscription/create-update-subscription/create-update-subscription.component.ts
+++ src/app/modules/subscription/components/assigned-subscription/create-update-subscription/create-update-subscription.component.ts
@@ -494,7 +494,8 @@ export class CreateUpdateSubscriptionComponent implements OnInit, OnDestroy {
   getOwnerFiler(){
     // https://api.taxbuddy.com/user/agent-assignment-new?userId=747677&assessmentYear=2023-2024&serviceType=ITR
     this.loading = true;
-    const param = `/agent-assignment-new?userId=${this.subscriptionObj.userId}&assessmentYear=${this.AssessmentYear}&serviceType=${this.serviceType}`;
+    let sType = this.serviceType === 'ITRU' ? 'ITR' : this.serviceType;
+    const param = `/agent-assignment-new?userId=${this.subscriptionObj.userId}&assessmentYear=${this.AssessmentYear}&serviceType=${sType}`;
     this.userService.getMethod(param).subscribe((result: any) => {
       this.loading = false;
       console.log('get Owner and filer name for new create sub ',result)
