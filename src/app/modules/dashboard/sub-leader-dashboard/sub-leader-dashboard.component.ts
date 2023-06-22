f7580c77cddf92e56d1632634056e031faff5cf7
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-06-22 11:52:52 +0530

    Fixed : leader dashboard on page load api call issue (partner commission api)

--- src/app/modules/dashboard/sub-leader-dashboard/sub-leader-dashboard.component.ts
+++ src/app/modules/dashboard/sub-leader-dashboard/sub-leader-dashboard.component.ts
@@ -191,19 +191,19 @@ export class SubLeaderDashboardComponent implements OnInit {
     let param=''
     let userFilter = '';
     if (this.ownerId && !this.filerId) {
-      userFilter += `ownerUserId=${this.ownerId}`;
+      userFilter += `&ownerUserId=${this.ownerId}`;
     }
-    else if (this.filerId) {
-      userFilter += `filerUserId=${this.filerId}`;
+    if (this.filerId) {
+      userFilter += `&filerUserId=${this.filerId}`;
     }
-    else{
-      userFilter += `leaderUserId=${this.loggedInSmeUserId}`;
-    }
+    // else{
+    //   userFilter += `leaderUserId=${this.loggedInSmeUserId}`;
+    // }
 
     if(this.filerId){
-      param = `/dashboard/partner-commission?${userFilter}&fromDate=${fromDate}&toDate=${toDate}`;
+      param = `/dashboard/partner-commission?fromDate=${fromDate}&toDate=${toDate}${userFilter}`;
     }else{
-      param = `/dashboard/partner-commission-cumulative?${userFilter}&fromDate=${fromDate}&toDate=${toDate}`;
+      param = `/dashboard/partner-commission-cumulative?fromDate=${fromDate}&toDate=${toDate}${userFilter}`;
     }
 
     this.userMsService.getMethodNew(param).subscribe(
