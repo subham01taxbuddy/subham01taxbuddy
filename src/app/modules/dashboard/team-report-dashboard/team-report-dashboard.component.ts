cba7087793e36a71b1ff4d79c2f7406da5f847b2
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-06-25 11:47:28 +0530

    Fixed:Leader Dashboard API changes (user to  report service) for sme-report & partnersAssignment

--- src/app/modules/dashboard/team-report-dashboard/team-report-dashboard.component.ts
+++ src/app/modules/dashboard/team-report-dashboard/team-report-dashboard.component.ts
@@ -154,7 +154,7 @@ export class TeamReportDashboardComponent implements OnInit {
 
     param = `/dashboard/sme-report${userFilter}`
 
-    this.userMsService.getMethod(param).subscribe((response: any) => {
+    this.userMsService.getMethodNew(param).subscribe((response: any) => {
       this.loading = false;
       if (response.success) {
          this.operationTeamData = response.data;
@@ -187,7 +187,7 @@ export class TeamReportDashboardComponent implements OnInit {
 
     param = `/dashboard/filers-partnerAssignment?fromDate=${fromDate}&toDate=${toDate}&serviceType=ITR${userFilter}`
 
-    this.userMsService.getMethod(param).subscribe((response: any) => {
+    this.userMsService.getMethodNew(param).subscribe((response: any) => {
       this.loading = false;
       if (response.success) {
          this.partnersAssignmentData = response.data;
