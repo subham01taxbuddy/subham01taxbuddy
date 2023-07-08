5d500b9797b0209f961702628c0a28b2ce50e95a
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-07-08 17:09:52 +0530

    Added new screen for payout adjustment and changes in bo-partners to add fundid/contactid

--- src/app/pages/bo-partners/update-status/update-status.component.ts
+++ src/app/pages/bo-partners/update-status/update-status.component.ts
@@ -3,6 +3,9 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ViewDocumentsComponent } from '../view-documents/view-documents.component';
 import { ToastMessageService } from 'src/app/services/toast-message.service';
 import { GstDailyReportService } from 'src/app/services/gst-daily-report.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserMsService } from 'src/app/services/user-ms.service';
+import { ItrMsService } from 'src/app/services/itr-ms.service';
 declare function we_track(key: string, value: any);
 
 @Component({
@@ -20,17 +23,92 @@ export class UpdateStatusComponent implements OnInit {
     { key: 'Document Pending', value: 'DOCUMENT_PENDING' },
     { key: 'Paid', value: 'PAID' },
   ];
+  myForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<ViewDocumentsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private _toastMessageService: ToastMessageService,
-    private gstreportService: GstDailyReportService
+    private gstreportService: GstDailyReportService,
+    private userMsService: UserMsService,
+    private itrMsService: ItrMsService,
   ) { }
 
   ngOnInit() {
     console.log(this.data);
+
+    if(this.data.mode === 'Update Information'){
+      this.getPartnerDetails();
+    }
+
+    this.myForm = new FormGroup({
+      name: new FormControl(this.data.partnerName),
+      mobileNo: new FormControl(this.data.mobileNumber),
+      contactId: new FormControl(''),
+      fundId: new FormControl('')
+    });
   }
+
+  getPartnerDetails(){
+  // https://api.taxbuddy.com/user/partner-details?mobileNumber=9023056993
+  this.loading=true;
+  let param =`/partner-details?mobileNumber=${this.data.mobileNumber}`
+  this.userMsService.getMethod(param).subscribe(
+    (response: any) => {
+      this.loading = false;
+      if (response){
+        this.loading = false;
+        this.myForm.get('contactId')?.setValue(response.contactId);
+        this.myForm.get('fundId')?.setValue(response.fundId);
+      } else {
+        this.loading = false;
+        this._toastMessageService.alert("error",'No Data Found');
+      }
+    },
+    (error) => {
+      this.loading = false;
+      this._toastMessageService.alert("error",'No Data Found');
+    }
+  );
+  }
+
+  updateDetails(){
+    //'https://7nlo6vqc673gcqlt7dx5byvgo40qcfeg.lambda-url.ap-south-1.on.aws/?mobileNumber=4570459025' \
+    const contactIdValue = this.myForm.get('contactId').value;
+    const fundIdValue = this.myForm.get('fundId').value;
+
+    if (contactIdValue !== null || fundIdValue !== null) {
+       this.loading = true;
+
+       const request = {
+        contactId: contactIdValue,
+        fundId : fundIdValue,
+      };
+      let param =`?mobileNumber=${this.data.mobileNumber}`;
+      this.itrMsService.putLambdaForUpdateId(param,request).subscribe((response: any) => {
+        this.loading = false;
+        if (response.success) {
+          this.loading = false;
+          console.log('response', response['data']);
+          this._toastMessageService.alert('success',response.message);
+        }else{
+          this.loading = false;
+          this._toastMessageService.alert('error','There is some issue to Update information.');
+        }
+        setTimeout(() => {
+          this.dialogRef.close({ event: 'close' })
+        }, 2500)
+      },(error) => {
+        this.loading = false;
+        this._toastMessageService.alert('error','There is some issue to Update information.');
+      });
+
+    } else {
+      this._toastMessageService.alert("error",'please provide fund Id or contact Id');
+    }
+
+  }
+
   addStatus() {
     if (this.data.mode === 'Update Status') {
       let param = '/partner-status';
