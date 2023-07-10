3ce4ed4b9647a11b5f6ef3aa50e10fd2edcff62d
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-07-10 13:04:47 +0530

    Fixed :changes in payout adjustment screen

--- src/app/modules/payouts/payout-adjustment/payout-adjustment.component.ts
+++ src/app/modules/payouts/payout-adjustment/payout-adjustment.component.ts
@@ -17,7 +17,7 @@ export class PayoutAdjustmentComponent implements OnInit {
   reason = new FormControl('');
   dialogRef: any;
   name: any;
-  showAdd =false;
+  showAdd = false;
 
   constructor(
     private itrMsService: ItrMsService,
@@ -36,12 +36,12 @@ export class PayoutAdjustmentComponent implements OnInit {
       let param = `?userId=${userId}`;
       this.itrMsService.getAdjustmentDetails(param).subscribe(
         (response: any) => {
+          this.showAdd =true;
           this.loading = false;
           if (response.success) {
             this.loading = false;
             this.adjustmentDetails = response.data;
             this.name = this.adjustmentDetails?.name;
-            this.showAdd =true;
             console.log('data for adjustment', this.adjustmentDetails);
           } else {
             this.loading = false;
@@ -69,6 +69,7 @@ export class PayoutAdjustmentComponent implements OnInit {
   ownerId: number;
   filerId: number;
   agentId: number;
+  partnerName: any;
 
   fromSme(event, isOwner) {
     console.log('sme-drop-down', event, isOwner);
@@ -76,6 +77,7 @@ export class PayoutAdjustmentComponent implements OnInit {
       this.ownerId = event ? event.userId : null;
     } else {
       this.filerId = event ? event.userId : null;
+      this.partnerName = event ? event.name : null;
     }
     if (this.filerId) {
       this.agentId = this.filerId;
@@ -88,20 +90,26 @@ export class PayoutAdjustmentComponent implements OnInit {
   }
 
   addAdjustment(templateRef) {
-    this.dialogRef = this.dialog.open(templateRef, {
-      width: 'auto',
-      data: {
-        name: this.name,
-      },
-    });
+    if (this.filerId && this.showAdd == true) {
+      this.dialogRef = this.dialog.open(templateRef, {
+        width: 'auto',
+        data: {
+          name: this.name,
+        },
+      });
 
-    this.dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        if (result.data === "added") {
-          this.search();
+      this.dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
+          if (result.data === 'added') {
+            this.search();
+          }
         }
-      }
-    });
+      });
+    } else {
+      this.utilsService.showSnackBar(
+        'Please select filer name and search to Add Adjustment'
+      );
+    }
   }
 
   saveAdjustment(amount, reason) {
@@ -130,7 +138,7 @@ export class PayoutAdjustmentComponent implements OnInit {
           );
         }
         setTimeout(() => {
-          this.dialogRef.close({ event: 'close',data: 'added' });
+          this.dialogRef.close({ event: 'close', data: 'added' });
         }, 2000);
       },
       (error) => {
@@ -140,6 +148,23 @@ export class PayoutAdjustmentComponent implements OnInit {
         );
       }
     );
+  }
+
+  calculateTotal(): number {
+    let total = 0;
+    if (this.adjustmentDetails?.adjustmentHistory) {
+      for (const item of this.adjustmentDetails?.adjustmentHistory) {
+        total += item.commisionAdjusted;
+      }
+      return total;
+    }
+  }
+  calculateAdditionTotal(): number {
+    let total = 0;
+    for (const item of this.adjustmentDetails?.adjustmentAdditions) {
+      total += item.adjustmentadditionAmount;
+    }
+    return total;
   }
 
   @ViewChild('smeDropDown') smeDropDown: SmeListDropDownComponent;
