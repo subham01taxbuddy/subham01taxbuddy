d0dc782e9b52b57fdcb904f3e524ceea0ea3e1d4
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-07-14 11:43:54 +0530

    Added Adjustment API in pay-processing

--- src/app/services/itr-ms.service.ts
+++ src/app/services/itr-ms.service.ts
@@ -76,6 +76,16 @@ export class ItrMsService {
     // .map(response => response.json())
   }
 
+  addAdjustment<T>(...param: any): Observable<T> {
+    this.headers = new HttpHeaders();
+    this.headers.append('Content-Type', 'application/json');
+    // this.headers.append('Authorization', 'Bearer ' + this.TOKEN);
+    return this.httpClient.get<T>(environment.adjustment + param[0],
+      { headers: this.headers }
+    );
+    // .map(response => response.json())
+  }
+
   getTdsDetails<T>(...param: any): Observable<T> {
     this.headers = new HttpHeaders();
     this.headers.append('Content-Type', 'application/json');
