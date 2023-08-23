61ad4fc7dfe5146fdb167e17857fc0b572457d5b
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-08-23 13:35:16 +0530

    fixed: shown error message on UI  TB2-2865

--- src/app/modules/sme-management-new/components/assigned-sme/edit-update-assigned-sme/edit-update-assigned-sme.component.ts
+++ src/app/modules/sme-management-new/components/assigned-sme/edit-update-assigned-sme/edit-update-assigned-sme.component.ts
@@ -569,8 +569,9 @@ export class EditUpdateAssignedSmeComponent implements OnInit {
         this.loading = false;
         this.utilsService.showSnackBar(result.message);
       }
-    }, (error)=>{
+    },(error)=>{
       this.loading = false;
+      this.utilsService.showSnackBar(error.error.message);
       console.log(error);
     });
   }
