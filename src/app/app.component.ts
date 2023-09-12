47fdb03e098536dc2b12bbdebf90bd731cafeddb
Ashwini <ashwini@ssbainnovations.com>
2023-09-12 10:07:41 +0530

    Removed gyde & kommunicate. Added freshdesk scripts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,43 +59,6 @@ export class AppComponent {
         }
       });
 
-    (function (d, m) {
-      var kommunicateSettings =
-      {
-        "appId": "3eb13dbd656feb3acdbdf650efbf437d1",
-        "popupWidget": true,
-        "automaticChatOpenOnNavigation": true,
-        "preLeadCollection":
-          [
-            {
-              "field": "Name", // Name of the field you want to add
-              "required": true, // Set 'true' to make it a mandatory field
-              "placeholder": "Enter your name" // add whatever text you want to show in the placeholder
-            },
-            {
-              "field": "Email",
-              "type": "email",
-              "required": true,
-              "placeholder": "Enter your email"
-            },
-            {
-              "field": "Phone",
-              "type": "number",
-              "required": true,
-              "element": "input", // Optional field (Possible values: textarea or input)
-              "placeholder": "Enter your phone number"
-            }
-          ],
-
-      };
-
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-       (window as any).kommunicate = m; m._globals = kommunicateSettings;
-    }
-    )(document,  (window as any).kommunicate || {});
-
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
         this.reloadWindow();
@@ -189,43 +152,6 @@ export class AppComponent {
   logout() {
     Auth.signOut()
       .then(data => {
-        (window as any).Kommunicate.logout();
-        (function (d, m) {
-            var kommunicateSettings =
-              {
-                "appId": "3eb13dbd656feb3acdbdf650efbf437d1",
-                "popupWidget": true,
-                "automaticChatOpenOnNavigation": true,
-                "preLeadCollection":
-                  [
-                    {
-                      "field": "Name", // Name of the field you want to add
-                      "required": true, // Set 'true' to make it a mandatory field
-                      "placeholder": "Enter your name" // add whatever text you want to show in the placeholder
-                    },
-                    {
-                      "field": "Email",
-                      "type": "email",
-                      "required": true,
-                      "placeholder": "Enter your email"
-                    },
-                    {
-                      "field": "Phone",
-                      "type": "number",
-                      "required": true,
-                      "element": "input", // Optional field (Possible values: textarea or input)
-                      "placeholder": "Enter your phone number"
-                    }
-                  ],
-
-              };
-
-            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-            (window as any).kommunicate = m; m._globals = kommunicateSettings;
-          }
-        )(document,  (window as any).kommunicate || {});
 
         sessionStorage.clear();
         NavbarService.getInstance().clearAllSessionData();
