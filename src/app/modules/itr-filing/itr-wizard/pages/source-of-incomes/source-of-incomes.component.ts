0848b72a62d3b177702ec7df5ebc16bd33d83c6a
Ashwini <ashwini@ssbainnovations.com>
2023-10-10 11:47:24 +0530

    Removed FSI/TR/FA/VDA from UI

--- src/app/modules/itr-filing/itr-wizard/pages/source-of-incomes/source-of-incomes.component.ts
+++ src/app/modules/itr-filing/itr-wizard/pages/source-of-incomes/source-of-incomes.component.ts
@@ -85,23 +85,24 @@ export class SourceOfIncomesComponent implements OnInit {
               : false,
           schedule: this.schedules.SPECULATIVE_INCOME,
         },
-        {
-          name: 'Crypto',
-          selected:
-            this.ITR_JSON.capitalGain != null &&
-            this.ITR_JSON.capitalGain.length > 0
-              ? true
-              : false,
-          schedule: this.schedules.CRYPTO_VDA,
-        },
-        {
-          name: 'Foreign Income',
-          selected:
-            this.ITR_JSON.foreignIncome && this.ITR_JSON.foreignIncome != null
-              ? true
-              : false,
-          schedule: this.schedules.FOREIGN_INCOME,
-        },
+        //Ashwini: Commented the crypto and foreign income as QA is not complete
+        // {
+        //   name: 'Crypto',
+        //   selected:
+        //     this.ITR_JSON.capitalGain != null &&
+        //     this.ITR_JSON.capitalGain.length > 0
+        //       ? true
+        //       : false,
+        //   schedule: this.schedules.CRYPTO_VDA,
+        // },
+        // {
+        //   name: 'Foreign Income',
+        //   selected:
+        //     this.ITR_JSON.foreignIncome && this.ITR_JSON.foreignIncome != null
+        //       ? true
+        //       : false,
+        //   schedule: this.schedules.FOREIGN_INCOME,
+        // },
       ];
       sessionStorage.setItem('incomeSources', JSON.stringify(this.sourcesList));
     } else {
