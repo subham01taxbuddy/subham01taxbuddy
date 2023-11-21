e3d93b93fb7c277f67923c785f0f909d478a8e3b
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2023-11-21 17:10:57 +0530

    Fixed: change in name of ITRs tab to Filed ITRs

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -89,7 +89,7 @@ export class SidebarComponent {
         },
         { name: 'ITR Assigned Users', url: '/tasks/itr-assigned-users', roles: [] },
         { name: 'Scheduled Calls', url: '/tasks/schedule-call', roles: ['ROLE_ADMIN', 'ROLE_LEADER'] },
-        { name: 'ITRs', url: '/tasks/filings', roles: [] },
+        { name: 'Filed ITRs', url: '/tasks/filings', roles: [] },
         // {
         //   name: 'Exceptions', url: '/tasks/exceptions', roles: [],
         //   // submenu: [
