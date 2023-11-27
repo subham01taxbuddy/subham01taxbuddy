1747ca34dba70b64e538c70ed011ad2ed530d9ba
Sushama Aher <sushma.aher@ssbainnovations.com>
2023-11-27 08:34:35 +0530

    added fix for duplicate communicate iframe

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -434,12 +434,19 @@ export class LoginComponent implements OnInit {
     const baseUrl = "https://dashboard-proxy.kommunicate.io";
     const userEmail = loginSmeDetails[0].email;
     const userAccessToken = `${token}&appId=${environment.kmAppId}`;
-    let iframe = document.createElement("iframe");
+    let iframe = document.getElementById('km-iframe') as HTMLIFrameElement;
+    if (!iframe) {
+      iframe = document.createElement("iframe");
+      iframe.setAttribute('class', 'iframe-height');
+      iframe.setAttribute('id', 'km-iframe');
+    }
+    
+    
     iframe.setAttribute('src', `${baseUrl}/login?email=${userEmail}&password=${userAccessToken}?showConversationSectionOnly=true`)
-    iframe.setAttribute('class', 'iframe-height');
-    iframe.setAttribute('id', 'km-iframe');
-    let viewbox = document.getElementById('km-viewbox');
-    viewbox.append(iframe);
+    if (!document.getElementById('km-iframe')) {
+      let viewbox = document.getElementById('km-viewbox');
+      viewbox.append(iframe);
+    }
   }
 
   mode: string = 'SIGN_IN';
