8ccc156624efa709ea47e09ca9ceb4b6c14303d9
Ashwini <ashwini@ssbainnovations.com>
2023-11-29 22:53:34 +0530

    Added validate api integration

--- src/app/modules/itr-filing/itr-wizard/itr-wizard.component.ts
+++ src/app/modules/itr-filing/itr-wizard/itr-wizard.component.ts
@@ -192,6 +192,20 @@ export class ItrWizardComponent implements OnInit {
     this.ITR_JSON = JSON.parse(sessionStorage.getItem(AppConstants.ITR_JSON));
     // if (this.jsonUploaded) {
 
+    this.itrMsService.getMethod(`/validate/${this.ITR_JSON.itrId}`).subscribe((result:any)=>{
+      console.log(result);
+      // if(result.success){
+        if (result.data.errors.length > 0) {
+          this.breadcrumb = null;
+          this.showIncomeSources = false;
+          this.selectedSchedule = 'Validation Errors';
+          this.router.navigate(['/itr-filing/itr/validation-errors'], {
+            state: { apiErrors: result.data.errors },
+          });
+        }
+      // }
+    });
+
     if (this.validationErrors?.length > 0) {
       this.breadcrumb = null;
       this.showIncomeSources = false;
