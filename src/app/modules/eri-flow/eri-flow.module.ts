656a1c4883a0c2fabaf1ade14f7a5b91b5bd6d55
Ashwini <ashwini@ssbainnovations.com>
2023-12-08 21:43:00 +0530

    Removed unused files

--- src/app/modules/eri-flow/eri-flow.module.ts
+++ src/app/modules/eri-flow/eri-flow.module.ts
@@ -4,41 +4,17 @@ import { SharedModule } from "src/app/modules/shared/shared.module";
 import { NgxLoadingModule } from "ngx-loading";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { EriFlowRoutingModule } from "./eri-flow.routing";
-import { DirectFilingComponent } from "./pages/direct-filing/direct-filing.component";
 import { AddClientComponent } from "./components/add-client/add-client.component";
 import { PrefillDataComponent } from "./components/prefill-data/prefill-data.component";
-import { GenerateSummaryComponent } from "./components/generate-summary/generate-summary.component";
-import { SubmitFilingComponent } from "./components/submit-filing/submit-filing.component";
 import { EVerifyComponent } from "./components/e-verify/e-verify.component";
-import { SumaryDialogComponent } from "./components/sumary-dialog/sumary-dialog.component";
-import { Itr4partComponent } from "./components/itr4part/itr4part.component";
-import { Itr2mainComponent } from "./components/itr2main/itr2main.component";
-import { NewItrSummaryComponent } from "./components/new-itr-summary/new-itr-summary.component";
-import { ItrOneComponent } from "./components/new-itr-summary/itr-one/itr-one.component";
-import { ItrThreeComponent } from "./components/new-itr-summary/itr-three/itr-three.component";
-import { TaxSummaryComponent } from "./components/tax-summary/tax-summary.component";
-import { SuccessSubmitFilingComponent } from "./components/success-submit-filing/success-submit-filing.component";
 import { ItrSharedModule } from "../itr-shared/itr-shared.module";
 import {PagesModule} from "../../pages/pages.module";
 
 @NgModule({
     declarations: [
-        DirectFilingComponent,
         AddClientComponent,
         PrefillDataComponent,
-        GenerateSummaryComponent,
-        SubmitFilingComponent,
-        EVerifyComponent,
-        // TaxSummaryComponent
-
-        TaxSummaryComponent,
-        SumaryDialogComponent,
-        Itr4partComponent,
-        Itr2mainComponent,
-        NewItrSummaryComponent,
-        ItrOneComponent,
-        ItrThreeComponent,
-        SuccessSubmitFilingComponent
+        EVerifyComponent
     ],
     imports: [
         CommonModule,
@@ -49,8 +25,7 @@ import {PagesModule} from "../../pages/pages.module";
         NgxLoadingModule.forRoot({}),
         ItrSharedModule,
         PagesModule
-    ],
-    entryComponents: [SuccessSubmitFilingComponent]
+    ]
 })
 
 export class EriFlowModule { }
