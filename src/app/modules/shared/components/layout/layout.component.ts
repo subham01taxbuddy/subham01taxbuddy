c78a85d6fb37393772e0fbf4cae3155d87509c9f
Sushama Aher <sushma.aher@ssbainnovations.com>
2023-12-20 15:50:44 +0530

    Completed sidebar showing image icons first version

--- src/app/modules/shared/components/layout/layout.component.ts
+++ src/app/modules/shared/components/layout/layout.component.ts
@@ -58,7 +58,6 @@ export class LayoutComponent implements OnInit {
   detectSidebar() {
     this.subscription = this.sidebarService.isLoading
       .subscribe((state) => {
-        debugger
         if (state) {
           this.openSidebar = true;
         } else {
