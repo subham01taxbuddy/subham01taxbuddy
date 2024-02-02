870961f1acc73fe9bfdf2cab6fc0ca9511a06481
Sushama Aher <sushma.aher@ssbainnovations.com>
2024-02-02 15:24:35 +0530

    Fixed presumptive income pagination issues

--- src/app/modules/itr-filing/business-income/presumptive-income/presumptive-professional-income/presumptive-professional-income.component.ts
+++ src/app/modules/itr-filing/business-income/presumptive-income/presumptive-professional-income/presumptive-professional-income.component.ts
@@ -59,10 +59,8 @@ export class PresumptiveProfessionalIncomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.config = {
-      id: 'professionConfig',
       itemsPerPage: 2,
       currentPage: 1,
-      totalItems: 0,
     };
 
     let profBusiness = this.ITR_JSON.business?.presumptiveIncomes?.filter(
@@ -86,7 +84,6 @@ export class PresumptiveProfessionalIncomeComponent implements OnInit {
     this.profIncomeFormArray.controls.forEach((formgroup, index) => {
       this.calculatePresumptive(null, index, false);
     });
-    this.config.totalItems = this.profIncomeFormArray.controls.length;
   }
 
   get getProfIncomeArray() {
@@ -155,14 +152,9 @@ export class PresumptiveProfessionalIncomeComponent implements OnInit {
   }
 
   deleteArray() {
-    const profIncomeFormArray = <FormArray>(
-      this.profIncomeForm.get('profIncomeFormArray')
-    );
-    profIncomeFormArray.controls = profIncomeFormArray.controls.filter(
-      (element) => !(element as FormGroup).controls['hasEdit'].value
-    );
-    this.profIncomeForm.controls['profIncomeFormArray'].setValue(profIncomeFormArray.controls);
-    this.config.totalItems = profIncomeFormArray.controls.length;
+    const profIncomeFormArray = <FormArray>this.profIncomeForm.get('profIncomeFormArray');
+    profIncomeFormArray.controls = profIncomeFormArray.controls.filter(element => !(element as FormGroup).controls['hasEdit'].value);
+
   }
 
   calculatePresumptive(event, index, setValue?) {
