b38133150cf415beef5de3330ee79b3e3146a1c3
Sushama Aher <sushma.aher@ssbainnovations.com>
2024-03-04 10:36:59 +0530

    completed presumptive income tabular format

--- src/app/modules/itr-filing/components/business-input-autocomplete/business-input-autocomplete.component.ts
+++ src/app/modules/itr-filing/components/business-input-autocomplete/business-input-autocomplete.component.ts
@@ -1,14 +1,14 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {ItrMsService} from "../../../../services/itr-ms.service";
-import {UtilsService} from "../../../../services/utils.service";
-import {FormControl} from "@angular/forms";
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { ItrMsService } from "../../../../services/itr-ms.service";
+import { UtilsService } from "../../../../services/utils.service";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-business-input-autocomplete',
   templateUrl: './business-input-autocomplete.component.html',
   styleUrls: ['./business-input-autocomplete.component.scss']
 })
-export class BusinessInputAutocompleteComponent implements OnInit {
+export class BusinessInputAutocompleteComponent implements OnInit, OnChanges {
 
   @ViewChild('input') input: ElementRef<HTMLInputElement>;
   myControl = new FormControl('');
@@ -32,11 +32,11 @@ export class BusinessInputAutocompleteComponent implements OnInit {
       sessionStorage.getItem('NATURE_OF_BUSINESS')
     );
     if (natureOfBusiness) {
-      if(this.businessType !== 'ALL') {
+      if (this.businessType !== 'ALL') {
         this.natureOfBusinessList = natureOfBusiness.filter(
           (item: any) => item.section === this.businessType
         );
-      } else{
+      } else {
         this.natureOfBusinessList = natureOfBusiness;
       }
       this.filteredOptions = this.natureOfBusinessList;
@@ -44,7 +44,13 @@ export class BusinessInputAutocompleteComponent implements OnInit {
       console.log('business list not found');
       this.getMastersData();
     }
-    if(this.selectedInput) {
+    if (this.selectedInput) {
+      this.myControl.setValue(this.selectedInput);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedInput']) {
       this.myControl.setValue(this.selectedInput);
     }
   }
@@ -58,11 +64,11 @@ export class BusinessInputAutocompleteComponent implements OnInit {
           'NATURE_OF_BUSINESS',
           JSON.stringify(natureOfBusinessAll)
         );
-        if(this.businessType !== 'ALL') {
+        if (this.businessType !== 'ALL') {
           this.natureOfBusinessList = natureOfBusinessAll.filter(
             (item: any) => item.section === this.businessType
           );
-        } else{
+        } else {
           this.natureOfBusinessList = natureOfBusinessAll;
         }
         sessionStorage.setItem('MASTER', JSON.stringify(result));
@@ -76,23 +82,23 @@ export class BusinessInputAutocompleteComponent implements OnInit {
     );
   }
 
-  optionSelected(event){
+  optionSelected(event) {
     console.log(event);
     this.inputSelection = true;
     this.selectedInput = event.option.value;
     this.businessSelected.emit(this.selectedInput);
   }
 
-  closed(event){
-    if(!this.inputSelection) {
+  closed(event) {
+    if (!this.inputSelection) {
       this.inputSelection = false;
       this.selectedInput = null;
       this.businessSelected.emit(this.selectedInput);
     }
   }
 
-  focusIn(event){
-    if(!this.inputSelection) {
+  focusIn(event) {
+    if (!this.inputSelection) {
       this.input.nativeElement.value = "";
       this.inputSelection = false;
     }
@@ -104,8 +110,8 @@ export class BusinessInputAutocompleteComponent implements OnInit {
       o => o.label.toLowerCase().includes(filterValue) || o.code.includes(filterValue));
   }
 
-  displayFn = (business: any): string  => {
-    if(business){
+  displayFn = (business: any): string => {
+    if (business) {
       let b = this.filteredOptions?.filter(o => o.code === business)[0];
       return b ? `${b?.label} - ${b?.code}` : '';
     } else {
