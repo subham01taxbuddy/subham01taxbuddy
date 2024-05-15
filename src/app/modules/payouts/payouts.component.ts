883d76147cdee87cd464b33902e0987bde395b0c
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2024-05-15 16:40:51 +0530

    Payout screen changed error message

--- src/app/modules/payouts/payouts.component.ts
+++ src/app/modules/payouts/payouts.component.ts
@@ -337,8 +337,8 @@ export class PayoutsComponent implements OnInit, OnDestroy {
       }
     }, error => {
       this.loading = false;
-      // this.utilsService.showSnackBar('Please try again, Data not found');
-      this.utilsService.showSnackBar(error.error.message);
+      this.utilsService.showSnackBar('Data not found');
+      // this.utilsService.showSnackBar(error.error.message);
       this.usersGridOptions.api?.setRowData([]);
       this.userInfo = [];
       this.config.totalItems = 0;
