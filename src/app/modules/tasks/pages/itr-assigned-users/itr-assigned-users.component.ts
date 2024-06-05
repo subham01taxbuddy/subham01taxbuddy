fb75b68074b250746a5425988e7043503981dca6
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2024-06-05 17:52:43 +0530

    ITR Assigned User financialYear change in check subscription

--- src/app/modules/tasks/pages/itr-assigned-users/itr-assigned-users.component.ts
+++ src/app/modules/tasks/pages/itr-assigned-users/itr-assigned-users.component.ts
@@ -394,10 +394,10 @@ export class ItrAssignedUsersComponent implements OnInit {
           }
 
           if (data.serviceType === 'ITRU') {
-            if (smeSelectedPlan && (smeSelectedPlan.servicesType === 'ITRU' && ((item1.financialYear === "2021-2022" || item1.financialYear === "2022-23")))) {
+            if (smeSelectedPlan && (smeSelectedPlan.servicesType === 'ITRU' && ((item1.financialYear === "2021-2022" || item1.financialYear === "2022-23" || item1.financialYear === "2022-2023")))) {
               itrSubscriptionFound = true;
               return;
-            } else if (userSelectedPlan && (userSelectedPlan.servicesType === 'ITRU' && ((item1.financialYear === "2021-2022" || item1.financialYear === "2022-23")))) {
+            } else if (userSelectedPlan && (userSelectedPlan.servicesType === 'ITRU' && ((item1.financialYear === "2021-2022" || item1.financialYear === "2022-23" || item1.financialYear === "2022-2023")))) {
               itrSubscriptionFound = true;
               return;
             }
