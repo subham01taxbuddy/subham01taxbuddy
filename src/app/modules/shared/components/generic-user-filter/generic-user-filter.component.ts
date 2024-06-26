5cfc79ebc75ee5d3bda5a4904a96fa4d5f7bf6f1
Sushama Aher <sushma.aher@ssbainnovations.com>
2024-06-26 11:33:45 +0530

    remove unused import

--- src/app/modules/shared/components/generic-user-filter/generic-user-filter.component.ts
+++ src/app/modules/shared/components/generic-user-filter/generic-user-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
 import { AppConstants } from '../../constants';
 
