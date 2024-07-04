a32ec13a2cdbf2cedc141fe249e26e5b94328f2f
rohitgaikwad6830 <rohit.gaikwad@qorbital.com>
2024-07-04 11:59:14 +0530

    Fixed: subscription code

--- src/app/modules/subscription/components/assigned-subscription/create-update-subscription/create-update-subscription.component.ts
+++ src/app/modules/subscription/components/assigned-subscription/create-update-subscription/create-update-subscription.component.ts
@@ -1319,89 +1319,114 @@ export class CreateUpdateSubscriptionComponent implements OnInit, OnDestroy, Aft
             this.utilsService.showSnackBar(res.error);
             this.loading = false;
             reject(res.error);
+            return;
           } else {
             if (this.selectedCouponCodeSubscriptionIds.length > 0 && (this.userSubscription?.payableSubscriptionAmount < 0 || this.userSubscription?.invoiceDetail?.some(invoice => invoice.paymentStatus === 'Paid'))) {
               this.utilsService.showSnackBar("If you apply a coupon code, it is not possible to generate a subscription with a negative amount.");
               this.loading = false;
               reject("Negative amount with coupon code");
-            } else if (this.service === 'ITRU' || this.service === 'ITR') {
-              if (this.assessmentYear.value === '' || typeof this.assessmentYear.value === 'undefined') {
+              return;
+            }
+
+            if (this.service === 'ITRU' || this.service === 'ITR') {
+              if (this.assessmentYear.value === '' || typeof this.assessmentYear.value === 'undefined' || this.assessmentYear.value === 'undefined') {
                 this.loading = false;
                 this.toastMessage.alert('error', 'Please select Financial Year For ' + (this.service === 'ITR' ? 'ITR' : 'ITR-U') + ' subscription');
-                reject("Missing Financial Year");
-              } else if (this.showScheduledFields === true && (this.scheduleCallService.value === '' || typeof this.scheduleCallService.value === 'undefined')) {
-                this.loading = false;
-                this.toastMessage.alert('error', 'Please Select Service Type For Scheduled Call');
-                reject("Missing Service Type");
-              } else if (this.userSubscription.smeSelectedPlan != null && this.pin.value) {
-                console.log('selectedPlanInfo -> ', this.userSubscription.smeSelectedPlan.planId);
-
-                if (!this.couponCodeAppliedFlag) this.selectedCouponCodeSubscriptionIds = [];
-
-                let param = '/subscription';
-                let reqBody: any = {
-                  userId: this.userSubscription.userId,
-                  planId: this.userSubscription.smeSelectedPlan.planId,
-                  selectedBy: 'SME',
-                  smeUserId: this?.loggedInSme[0]?.userId,
-                  item: {
-                    itemDescription: this.description?.value,
-                    quantity: this.userSubscription?.item[0]?.quantity,
-                    rate: this?.userSubscription?.payableSubscriptionAmount,
-                    cgstPercent: this?.userSubscription?.cgstPercent,
-                    cgstAmount: this?.userSubscription?.cgstAmount,
-                    igstAmount: this?.userSubscription?.igstAmount,
-                    igstPercent: this?.userSubscription?.igstPercent,
-                    sgstPercent: this?.userSubscription?.sgstPercent,
-                    sgstAmount: this?.userSubscription?.sgstAmount,
-                    amount: this?.userSubscription?.payableSubscriptionAmount,
-                    sacCode: this.sacNumber.value,
-                    financialYear: this.assessmentYear.value,
-                    service: this.service,
-                    serviceDetail: this.serviceDetail,
-                  },
-                  reminderEmail: this.reminderEmail.value,
-                  reminderMobileNumber: this.reminderMobileNumber.value,
-                  subscriptionId: this.subscriptionObj.subscriptionId,
-                  removePromoCode: this.isPromoRemoved,
-                  promoCode: this.selectedPromoCode,
-                  couponCodeSubscriptionIds: this.selectedCouponCodeSubscriptionIds,
-                  removeCouponCode: this.removeCouponCodeFlag
-                };
-
-                if (this.scheduleCallService.value) reqBody.serviceType = this.scheduleCallService.value;
-                if (this.scheduleCallType.value) reqBody.item.scheduleCallType = this.scheduleCallType.value;
-
-                console.log('Req Body: ', reqBody);
-                let requestData = JSON.parse(JSON.stringify(reqBody));
-
-                this.itrService.postMethod(param, requestData).subscribe(
-                  (res: any) => {
-                    this.loading = false;
-                    let invoiceTypeDetails = this.invoiceAmount > this.userSubscription?.payableSubscriptionAmount ? 'Downgrade' : 'Upgrade';
-
-                    this.toastMessage.alert('success', 'Subscription created successfully.');
-                    this.location.back();
-                    resolve('resolved');
-                  },
-                  (error) => {
-                    this.loading = false;
-                    if (error.error.error === 'BAD_REQUEST') {
-                      this.toastMessage.alert('error', error.error.message);
-                    } else {
-                      this.toastMessage.alert('error', this.utilsService.showErrorMsg(error.error.status));
-                    }
-                    this.router.navigate(['/subscription/assigned-subscription'], {
-                      queryParams: { fromEdit: true },
-                    });
-                    reject(error);
-                  }
-                );
-              } else {
-                this.toastMessage.alert('error', 'Please select Plan & Pincode');
-                this.loading = false;
-                reject("Missing Plan & Pincode");
+                return;
               }
+            }
+
+            if (this.showScheduledFields === true && (this.scheduleCallService.value === '' || typeof this.scheduleCallService.value === 'undefined' || this.scheduleCallService.value === 'undefined')) {
+              this.loading = false;
+              this.toastMessage.alert('error', 'Please Select Service Type For Scheduled Call ');
+              return;
+            }
+
+            if (this.userSubscription.smeSelectedPlan != null && this.pin.value) {
+              console.log(
+                'selectedPlanInfo -> ',
+                this.userSubscription.smeSelectedPlan.planId
+              );
+
+              if (!this.couponCodeAppliedFlag){
+                this.selectedCouponCodeSubscriptionIds = [];
+              }
+
+              let param = '/subscription';
+              let reqBody: any = {
+                userId: this.userSubscription.userId,
+                planId: this.userSubscription.smeSelectedPlan.planId,
+                selectedBy: 'SME',
+                smeUserId: this?.loggedInSme[0]?.userId,
+                item: {
+                  itemDescription: this.description?.value,
+                  quantity: this.userSubscription?.item[0]?.quantity,
+                  rate: this?.userSubscription?.payableSubscriptionAmount,
+                  cgstPercent: this?.userSubscription?.cgstPercent,
+                  cgstAmount: this?.userSubscription?.cgstAmount,
+                  igstAmount: this?.userSubscription?.igstAmount,
+                  igstPercent: this?.userSubscription?.igstPercent,
+                  sgstPercent: this?.userSubscription?.sgstPercent,
+                  sgstAmount: this?.userSubscription?.sgstAmount,
+                  amount: this?.userSubscription?.payableSubscriptionAmount,
+                  sacCode: this.sacNumber.value,
+                  financialYear: this.assessmentYear.value,
+                  service: this.service,
+                  serviceDetail: this.serviceDetail,
+                },
+                reminderEmail: this.reminderEmail.value,
+                reminderMobileNumber: this.reminderMobileNumber.value,
+                subscriptionId: this.subscriptionObj.subscriptionId,
+                removePromoCode: this.isPromoRemoved,
+                promoCode: this.selectedPromoCode,
+                couponCodeSubscriptionIds: this.selectedCouponCodeSubscriptionIds,
+                removeCouponCode: this.removeCouponCodeFlag
+              };
+
+              if (this.scheduleCallService.value) {
+                reqBody.serviceType = this.scheduleCallService.value;
+              }
+              if (this.scheduleCallService.value) {
+                reqBody.serviceType = this.scheduleCallService.value
+              }
+
+              if (this.scheduleCallType.value) {
+                reqBody.item.scheduleCallType = this.scheduleCallType.value;
+              }
+
+              console.log('Req Body: ', reqBody);
+              let requestData = JSON.parse(JSON.stringify(reqBody));
+
+              this.itrService.postMethod(param, requestData).subscribe(
+                (res: any) => {
+                  this.loading = false;
+                  let invoiceTypeDetails;
+                  if (this.invoiceAmount > this.userSubscription?.payableSubscriptionAmount) {
+                    invoiceTypeDetails = 'Downgrade';
+                  } else {
+                    invoiceTypeDetails = 'Upgrade';
+                  }
+                  this.toastMessage.alert('success', 'Subscription created successfully.');
+                  this.location.back();
+                  resolve('resolved');
+                },
+                (error) => {
+                  this.loading = false;
+                  if (error.error.error === 'BAD_REQUEST') {
+                    this.toastMessage.alert('error', error.error.message);
+                  } else {
+                    this.toastMessage.alert('error', this.utilsService.showErrorMsg(error.error.status));
+                  }
+                  this.router.navigate(['/subscription/assigned-subscription'], {
+                    queryParams: { fromEdit: true },
+                  });
+                  reject(error);
+                }
+              );
+            }else {
+              this.toastMessage.alert('error', 'Please select Plan & Pincode');
+              this.loading = false;
+              reject("Missing Plan & Pincode");
             }
           }
         },
