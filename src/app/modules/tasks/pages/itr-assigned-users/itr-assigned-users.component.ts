e37700b9853604f80bcdd199bdbf64db8fd438ac
Ashwini <ashwini@ssbainnovations.com>
2024-07-27 19:06:15 +0530

    Fix: corrected key

--- src/app/modules/tasks/pages/itr-assigned-users/itr-assigned-users.component.ts
+++ src/app/modules/tasks/pages/itr-assigned-users/itr-assigned-users.component.ts
@@ -556,7 +556,7 @@ export class ItrAssignedUsersComponent implements OnInit {
     // console.log(data);
     // console.log(Math.abs(moment(data.statusUpdatedDate).diff(moment.now()))/1000/60);
     let filteredPlans = ["Salary & House Property Plan", "Capital Gain Plan"]
-    return  !(data.serviceType === 'ITR' && !data.filerId && (!data.subscriptionPlan || filteredPlans.includes(data.subscriptionPlan))
+    return  !(data.serviceType === 'ITR' && !data.filerUserId && (!data.subscriptionPlan || filteredPlans.includes(data.subscriptionPlan))
         && Math.abs(moment(data.statusUpdatedDate).diff(moment.now()))/1000/60 <= AppConstants.DISABLITY_TIME_MINS);
   }
 
