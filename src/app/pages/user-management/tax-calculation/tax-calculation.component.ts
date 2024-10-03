565f0bb102f65df1cd4de8ce44c2e92722656ba2
subham01taxbuddy <subham.saha@taxbuddy.com>
2024-10-03 13:15:06 +0530

    fixed get api call in tax-calculation

--- src/app/pages/user-management/tax-calculation/tax-calculation.component.ts
+++ src/app/pages/user-management/tax-calculation/tax-calculation.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TaxDataService } from '../../../../app/tax-data.service';
 import { UserTaxDataService } from '../../../services/user-tax-data.service';
 
@@ -15,7 +14,8 @@ export class TaxCalculationComponent implements OnInit {
   userId: number | undefined;
   taxCalculationForm: FormGroup;
   toggleLabel: string = 'OFF';
-  apiUrl = 'https://uat-api.taxbuddy.com/itr/calculate/advance-tax'; // API URL
+  apiUrl = 'https://uat-api.taxbuddy.com/itr/calculate/advance-tax';
+  getApiUrl = 'https://uat-api.taxbuddy.com/itr/calculate/advance-tax';
 
   // Properties to hold response data
   advanceTaxQuarter1: any;
@@ -32,7 +32,7 @@ export class TaxCalculationComponent implements OnInit {
     private http: HttpClient,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private taxDataService: TaxDataService, // Inject the service
+    private taxDataService: TaxDataService,
     private userTaxDataService: UserTaxDataService
   ) {
     // Initialize the form group
@@ -52,7 +52,7 @@ export class TaxCalculationComponent implements OnInit {
       incomeFromOtherSources: [''],
       exemptionAllowedOldregime: [''],
       exemptionAllowedNewregime: [''],
-      toggleOption: [false, Validators.required], // Default is OFF
+      toggleOption: [false, Validators.required],
       ltcg112A: [''],
       ltcg112Other: [''],
       stcg111A: [''],
@@ -68,7 +68,7 @@ export class TaxCalculationComponent implements OnInit {
       eightyCCD2: [''],
       houseProperty: [''],
       businessNonSpeculative: [''],
-      BusinessSpeculative: [''],
+      businessSpeculative: [''],
       LTCG112A: [''],
       STCG111A: [''],
       LTCGother112A: [''],
@@ -76,8 +76,10 @@ export class TaxCalculationComponent implements OnInit {
       TDS: [''],
       AdvanceTaxIfAny: [''],
       anyOther: [''],
-      userId: [null], // This will be populated from queryParams
+      userId: [null],
       deductionAt30: [''],
+      us80ttattb: [''],
+      us80g80ggc80gga: [''],
     });
   }
 
@@ -85,40 +87,51 @@ export class TaxCalculationComponent implements OnInit {
     this.taxCalculationForm.controls['assessmentYear'].setValue('2025-2026');
     this.taxCalculationForm.controls['assessmentYear'].disable();
 
+    // Get userId from queryParams
     this.activatedRoute.queryParams.subscribe((params) => {
       this.userId = params['userId'];
-      console.log('User ID from query params:', this.userId);
       this.taxCalculationForm.patchValue({ userId: this.userId });
 
-      // Patch other values if available
-      this.taxCalculationForm.patchValue({
-        assesseeName: params['name'],
-        panNumber: params['pan'],
-        assessmentYear: params['assessmentYear'],
-        dateOfBirth: params['dob'],
-        grossSalary: params['grossSalary'],
-        exemptionAllowedOldregime: params['oldRegimeSalaryExemption'],
-        exemptionAllowedNewregime: params['newRegimeSalaryExemption'],
-        ltcg112A: params['ltcg112A'],
-        ltcg112Other: params['ltcg112Other'],
-        stcg111A: params['stcg111A'],
-        stcgAppRate: params['stcgAppRate'],
-        rentalIncome: params['rentalIncome'],
-        homeLoanInterest: params['homeLoanInterest'],
-        anyOther: params['anyOther'],
-        AdvanceTaxIfAny: params['advanceTaxPaid'],
-        incomeFromOtherSources: params['otherSourceIncome'],
-        nonSpeculative: params['pylNonSpeculativeIncome'],
-        speculative: params['pylSpeculativeIncome'],
-        houseProperty: params['pylHp'],
-        businessNonSpeculative: params['pylLtcgl12A'],
-        businessSpeculative: params['pylStcg111A'],
-        LTCG112A: params['pylLtcgOtherThan112A'],
-        STCG111A: params['pylStcgOtherThan111A'],
-        TDS: params['tdsTcs'],
-        deductionAt30: params['deduction'] || 0,
-      });
+      if (this.userId) {
+        this.fetchTaxData(this.userId);
+      }
     });
+  }
+
+  fetchTaxData(userId: number): void {
+    const apiUrl = `${this.apiUrl}/${userId}`;
+
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        const taxData = response.data;
+        this.taxCalculationForm.patchValue({
+          assesseeName: taxData.name,
+          panNumber: taxData.pan,
+          assessmentYear: taxData.assessmentYear,
+          dateOfBirth: taxData.dob,
+          grossSalary: taxData.grossSalary,
+          exemptionAllowedOldregime: taxData.oldRegimeSalaryExemption,
+          exemptionAllowedNewregime: taxData.newRegimeSalaryExemption,
+          rentalIncome: taxData.rentalIncome,
+          homeLoanInterest: taxData.homeLoanInterest,
+          anyOther: taxData.anyOther,
+          incomeFromOtherSources: taxData.otherSourceIncome,
+          nonSpeculative: taxData.pylNonSpeculativeIncome,
+          speculative: taxData.pylSpeculativeIncome,
+          houseProperty: taxData.pylHp,
+          businessNonSpeculative: taxData.pylLtcgl12A,
+          businessSpeculative: taxData.pylStcg111A,
+          LTCG112A: taxData.pylLtcgOtherThan112A,
+          STCG111A: taxData.pylStcgOtherThan111A,
+          TDS: taxData.tdsTcs,
+          AdvanceTaxIfAny: taxData.advanceTaxPaid,
+          deductionAt30: taxData.deduction,
+        });
+      },
+      (error) => {
+        console.error('Error fetching tax data:', error);
+      }
+    );
   }
 
   onToggleChange(checked: boolean): void {
@@ -126,6 +139,8 @@ export class TaxCalculationComponent implements OnInit {
   }
 
   onSubmit(): void {
+    const formData = this.buildFormData();
+
     const grossSalary = this.taxCalculationForm.get('grossSalary')?.value || 0;
     const oldRegimeExemption =
       this.taxCalculationForm.get('exemptionAllowedOldregime')?.value || 0;
@@ -184,70 +199,17 @@ export class TaxCalculationComponent implements OnInit {
     }
 
     if (this.taxCalculationForm.valid) {
-      const formData = {
-        userId: this.userId,
-        name: this.taxCalculationForm.get('assesseeName')?.value || '',
-        assessmentYear: '2025-2026',
-        pan: this.taxCalculationForm.get('panNumber')?.value || '',
-        dob: this.taxCalculationForm.get('dateOfBirth')?.value || '',
-        grossSalary: this.taxCalculationForm.get('grossSalary')?.value || 0,
-        otherSourceIncome:
-          this.taxCalculationForm.get('incomeFromOtherSources')?.value || 0,
-        oldRegimeSalaryExemption:
-          this.taxCalculationForm.get('exemptionAllowedOldregime')?.value || 0,
-        newRegimeSalaryExemption:
-          this.taxCalculationForm.get('exemptionAllowedNewregime')?.value || 0,
-        ltcg112A: this.taxCalculationForm.get('ltcg112A')?.value || 0,
-        ltcg112Other: this.taxCalculationForm.get('ltcg112Other')?.value || 0,
-        stcg111A: this.taxCalculationForm.get('stcg111A')?.value || 0,
-        stcgAppRate: this.taxCalculationForm.get('stcgAppRate')?.value || 0,
-        businessIncome:
-          this.taxCalculationForm.get('nonSpeculative')?.value || 0,
-        speculativeBusinessIncome:
-          this.taxCalculationForm.get('speculative')?.value || 0,
-        rentalIncome: this.taxCalculationForm.get('rentalIncome')?.value || 0,
-        homeLoanInterest:
-          this.taxCalculationForm.get('homeLoanInterest')?.value || 0,
-        sopHomeLoanInterest:
-          this.taxCalculationForm.get('selfOccupaid')?.value || 0,
-        us80c: this.taxCalculationForm.get('eightyCDD1')?.value || 0,
-        us80dSelf: this.taxCalculationForm.get('selfAndFamily')?.value || 0,
-        us80ccd1b: this.taxCalculationForm.get('eighty1B')?.value || 0,
-        us80ccd2: this.taxCalculationForm.get('eightyCCD2')?.value || 0,
-        us80ttattb: this.taxCalculationForm.get('houseProperty')?.value || 0,
-        pylNonSpeculativeIncome:
-          this.taxCalculationForm.get('businessNonSpeculative')?.value || 0,
-        pylSpeculativeIncome:
-          this.taxCalculationForm.get('BusinessSpeculative')?.value || 0,
-        pylLtcgl12A: this.taxCalculationForm.get('LTCG112A')?.value || 0,
-        pylStcg111A: this.taxCalculationForm.get('STCG111A')?.value || 0,
-        pylLtcgOtherThan112A:
-          this.taxCalculationForm.get('LTCGother112A')?.value || 0,
-        pylStcgOtherThan111A:
-          this.taxCalculationForm.get('STCGotherthan111A')?.value || 0,
-        tdsTcs: this.taxCalculationForm.get('TDS')?.value || 0,
-        advanceTaxPaid:
-          this.taxCalculationForm.get('AdvanceTaxIfAny')?.value || 0,
-        anyOther: this.taxCalculationForm.get('anyOther')?.value || 0,
-        deduction: this.taxCalculationForm.get('deductionAt30')?.value || 0,
-      };
-
-      console.log('Submitted Data: ', formData);
-
-      // Call the API
       this.http
         .post(this.apiUrl, formData, {
           headers: { 'Content-Type': 'application/json' },
         })
         .subscribe(
           (response: any) => {
-            console.log('API Response: ', response);
             if (response.success) {
               this.taxDataService.setTaxData(response.data);
               this.userTaxDataService.setUserTaxData(formData);
               this.handleApiResponse(response.data);
 
-              // Navigate to the TaxCalculationDetailsComponent
               this.router.navigate([
                 '/pages/user-management/tax-calculation-details',
               ]);
@@ -260,30 +222,66 @@ export class TaxCalculationComponent implements OnInit {
           }
         );
     } else {
-      // alert('At least one of the Income Source you have to enter.');
       console.log('Form is invalid or required fields are empty');
     }
   }
 
-  private handleApiResponse(data: any): void {
-    const {
-      advanceTaxQuarter1,
-      advanceTaxQuarter2,
-      advanceTaxQuarter3,
-      advanceTaxQuarter4,
-      oldRegime,
-      newRegime,
-      totalTaxLiability,
-      beneficialRegime,
-    } = data;
+  private buildFormData() {
+    return {
+      userId: this.userId,
+      name: this.taxCalculationForm.get('assesseeName')?.value || '',
+      assessmentYear: '2025-2026',
+      pan: this.taxCalculationForm.get('panNumber')?.value || '',
+      dob: this.taxCalculationForm.get('dateOfBirth')?.value || '',
+      grossSalary: this.taxCalculationForm.get('grossSalary')?.value || 0,
+      otherSourceIncome:
+        this.taxCalculationForm.get('incomeFromOtherSources')?.value || 0,
+      oldRegimeSalaryExemption:
+        this.taxCalculationForm.get('exemptionAllowedOldregime')?.value || 0,
+      newRegimeSalaryExemption:
+        this.taxCalculationForm.get('exemptionAllowedNewregime')?.value || 0,
+      ltcg112A: this.taxCalculationForm.get('ltcg112A')?.value || 0,
+      ltcg112Other: this.taxCalculationForm.get('ltcg112Other')?.value || 0,
+      stcg111A: this.taxCalculationForm.get('stcg111A')?.value || 0,
+      stcgAppRate: this.taxCalculationForm.get('stcgAppRate')?.value || 0,
+      businessIncome: this.taxCalculationForm.get('nonSpeculative')?.value || 0,
+      speculativeBusinessIncome:
+        this.taxCalculationForm.get('speculative')?.value || 0,
+      rentalIncome: this.taxCalculationForm.get('rentalIncome')?.value || 0,
+      homeLoanInterest:
+        this.taxCalculationForm.get('homeLoanInterest')?.value || 0,
+      sopHomeLoanInterest:
+        this.taxCalculationForm.get('selfOccupaid')?.value || 0,
+      us80c: this.taxCalculationForm.get('eightyCDD1')?.value || 0,
+      us80dSelf: this.taxCalculationForm.get('selfAndFamily')?.value || 0,
+      us80ccd1b: this.taxCalculationForm.get('eighty1B')?.value || 0,
+      us80ccd2: this.taxCalculationForm.get('eightyCCD2')?.value || 0,
+      us80ttattb: this.taxCalculationForm.get('houseProperty')?.value || 0,
+      pylNonSpeculativeIncome:
+        this.taxCalculationForm.get('businessNonSpeculative')?.value || 0,
+      pylSpeculativeIncome:
+        this.taxCalculationForm.get('businessSpeculative')?.value || 0,
+      pylLtcgl12A: this.taxCalculationForm.get('LTCG112A')?.value || 0,
+      pylStcg111A: this.taxCalculationForm.get('STCG111A')?.value || 0,
+      pylLtcgOtherThan112A:
+        this.taxCalculationForm.get('LTCGother112A')?.value || 0,
+      pylStcgOtherThan111A:
+        this.taxCalculationForm.get('STCGotherthan111A')?.value || 0,
+      tdsTcs: this.taxCalculationForm.get('TDS')?.value || 0,
+      advanceTaxPaid:
+        this.taxCalculationForm.get('AdvanceTaxIfAny')?.value || 0,
+      anyOther: this.taxCalculationForm.get('anyOther')?.value || 0,
+    };
+  }
 
-    this.advanceTaxQuarter1 = advanceTaxQuarter1;
-    this.advanceTaxQuarter2 = advanceTaxQuarter2;
-    this.advanceTaxQuarter3 = advanceTaxQuarter3;
-    this.advanceTaxQuarter4 = advanceTaxQuarter4;
-    this.oldRegime = oldRegime;
-    this.newRegime = newRegime;
-    this.totalTaxLiability = totalTaxLiability;
-    this.beneficialRegime = beneficialRegime;
+  private handleApiResponse(data: any): void {
+    this.totalTaxLiability = data.totalTaxLiability;
+    this.beneficialRegime = data.beneficialRegime;
+    this.advanceTaxQuarter1 = data.quarter1;
+    this.advanceTaxQuarter2 = data.quarter2;
+    this.advanceTaxQuarter3 = data.quarter3;
+    this.advanceTaxQuarter4 = data.quarter4;
+    this.oldRegime = data.oldRegimeTax;
+    this.newRegime = data.newRegimeTax;
   }
 }
