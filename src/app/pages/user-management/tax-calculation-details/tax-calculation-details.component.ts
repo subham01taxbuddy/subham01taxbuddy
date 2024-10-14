e15f1e194591ed53fb5e6d2d16a4384d05da9ffd
ajayjavkar <ajay.javkar@ssbainnovations.com>
2024-10-14 12:34:41 +0530

    Fix: advance tax calculation changes

--- src/app/pages/user-management/tax-calculation-details/tax-calculation-details.component.ts
+++ src/app/pages/user-management/tax-calculation-details/tax-calculation-details.component.ts
@@ -47,14 +47,19 @@ export class TaxCalculationDetailsComponent implements OnInit {
   advanceTaxQuarter2: any = {};
   advanceTaxQuarter3: any = {};
   advanceTaxQuarter4: any = {};
-  totalTaxLiabilty: string | undefined;
-  advanceTaxPaidQ1: string | undefined;
-  advanceTaxPaidQ2: string | undefined;
-  advanceTaxPaidQ3: string | undefined;
-  advanceTaxPaidQ4: string | undefined;
-  advanceTaxPaidQ5: string | undefined;
-       tdsTcs:       string|undefined;
-  advanceTaxPaid:   string | undefined; 
+  totalTaxLiabilty: number = 0;
+  advanceTaxPaidQ1: number = 0;
+  advanceTaxPaidQ2: number = 0;
+  advanceTaxPaidQ3: number = 0;
+  advanceTaxPaidQ4: number = 0;
+  advanceTaxPaidQ5: number = 0;
+  tdsTcs: number = 0;
+  advanceTaxPaid: number = 0;
+  taxLiability: number = 0;
+  taxPayable: number = 0;
+  totalAdvanceTaxPaid: number = 0;
+  interestUs234C: number = 0;
+  taxPayableOrRefund: any = '0';
 
   constructor(
     private router: Router,
@@ -87,7 +92,6 @@ export class TaxCalculationDetailsComponent implements OnInit {
       this.newRegime = data.newRegime;
       this.beneficialRegime = data.beneficialRegime;
       this.totalTaxLiabilty = data.totalTaxLiabilty;
-      this.tdsTcs = data.tdsTcs;
       this.advanceTaxPaid = data.advanceTaxPaid;
       this.advanceTaxPaidQ1 = data.advanceTaxPaidQ1;
       this.advanceTaxPaidQ2 = data.advanceTaxPaidQ2;
@@ -99,6 +103,22 @@ export class TaxCalculationDetailsComponent implements OnInit {
       this.advanceTaxQuarter2 = data.advanceTaxQuarter2;
       this.advanceTaxQuarter3 = data.advanceTaxQuarter3;
       this.advanceTaxQuarter4 = data.advanceTaxQuarter4;
+
+      let beneficialRegimeData;
+      if("New Regime" === data.beneficialRegime)
+        beneficialRegimeData = data.newRegime;
+      else
+        beneficialRegimeData = data.oldRegime;
+
+      this.tdsTcs = beneficialRegimeData.taxesPaid;
+      this.taxLiability = beneficialRegimeData.totalTax;
+      this.totalAdvanceTaxPaid = this.advanceTaxPaidQ1 + this.advanceTaxPaidQ2 + this.advanceTaxPaidQ3 + this.advanceTaxPaidQ4 + this.advanceTaxPaidQ5;
+      this.interestUs234C = this.advanceTaxQuarter1.interestUs234C + 
+      this.advanceTaxQuarter2.interestUs234C + 
+      this.advanceTaxQuarter3.interestUs234C + 
+      this.advanceTaxQuarter4.interestUs234C;
+      const taxpayable = this.taxLiability - this.tdsTcs - this.totalAdvanceTaxPaid + this.interestUs234C;
+      this.taxPayableOrRefund = taxpayable < 0 ? '('+Math.abs(taxpayable)+')' : taxpayable;
 
       this.calculateTotalIncome();
     }
